a3dfafa96d71b29219e202d947d0f283eec1d217
kyomukyomupurin <murakami1728@gmail.com>
2019-01-07 02:56:56 +0900

    create another 3 files

---
+++ src/fastinput.cpp
@@ -0,0 +1,16 @@
+/**
+ *  fast input
+**/
+
+void fastin(int &num) {
+    bool neg = false;
+    int c;
+    num = 0;
+    c = getchar_unlocked();
+    if (c == '-') {
+        neg = true;
+        c = getchar_unlocked();
+    }
+    for (; (c > 47 && c < 58); c = getchar_unlocked()) num = (num<<3) + (num<<1) + c - 48;
+    if (neg) num *= -1;
+}
---
+++ src/lcm.cpp
@@ -0,0 +1,14 @@
+/**
+ *  gcd(greatest common divisor), lcm(least common multiple)
+**/
+
+int64_t gcd(int64_t x, int64_t y){
+    if (x % y == 0) return y;
+    else return gcd(y, x % y);
+}
+
+int64_t lcm(int64_t x, int64_t y){
+    if (x % y == 0) return x;
+    else if (y % x == 0) return y;
+    else return x * y / gcd(x, y);
+}
