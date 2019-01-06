4f226eab2eb755d45e36432a02b97d75e1a31a3d
kyomukyomupurin <murakami1728@gmail.com>
2019-01-07 03:19:58 +0900

    template for competitive programming

---
+++ src/template.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+using int64 = int64_t;
+using pii = pair<int, int>;
+using pll = pair<int64_t, int64_t>;
+#define rep(i, n) for (int i = 0; i < (int)(n); ++i)
+#define all(v) (v).begin(), (v).end()
+#define print(x) cout << (x) << '\n'
+#define PB push_back
+#define EB emplace_back
+#define MP make_pair
+#define fi first
+#define se second
+
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    
+    
+    return 0;
+}
