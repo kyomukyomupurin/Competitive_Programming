133506da1942df72744f05a2730b2bb4dcaf2c60
kyomukyomupurin <murakami1728@gmail.com>
2019-01-07 03:01:43 +0900

    create another 2 files

---
+++ src/split-string.cpp
@@ -0,0 +1,17 @@
+/**
+ *  split string
+**/
+
+vector<string> split(const string &str, char sep) {
+    vector<string> v;
+    auto beg = str.begin();
+    while(beg != str.end()) {
+        auto end = beg;
+        while(end != str.end() && *end != sep)
+            ++end;
+        v.push_back(string(beg, end));
+        if(end != str.end()) ++end;
+        beg = end;
+    }
+    return v;
+}
