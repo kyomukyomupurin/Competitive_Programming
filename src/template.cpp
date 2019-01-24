8a08a4e415092782a854979f6f9c76386b6947dd
kyomukyomupurin <murakami1728@gmail.com>
2019-01-24 23:37:54 +0900

    minor changes

--- src/template.cpp
+++ src/template.cpp
@@ -1,8 +1,40 @@
-#include <bits/stdc++.h>
+/**
+ *      purpose : 
+ *      author  : kyomukyomupurin
+ *      created : 
+**/
+
+// in, out
+#include <iostream>
+// container class
+#include <array>
+#include <map>
+#include <queue>
+#include <set>
+#include <stack>
+#include <string>
+#include <unordered_map>
+#include <unordered_set>
+#include <vector>
+// math, algorithm
+#include <algorithm>
+#include <cmath>
+#include <complex>
+#include <numeric>
+// etc
+#include <bitset>
+#include <chrono>
+#include <iomanip>
+#include <random>
+#include <utility>
+// using-directive
+using namespace std;
+// alias templates
 using namespace std;
 using int64 = int64_t;
 using pii = pair<int, int>;
 using pll = pair<int64_t, int64_t>;
+// text macro replacement
 #define rep(i, n) for (int i = 0; i < (int)(n); ++i)
 #define all(v) (v).begin(), (v).end()
 #define print(x) cout << (x) << '\n'
