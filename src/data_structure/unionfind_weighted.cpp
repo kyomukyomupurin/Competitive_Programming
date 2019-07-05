854c81f016a96f0bba0223cab7c610352d045ba8
kyomukyomupurin <murakami1728@gmail.com>
2019-07-05 22:02:11 +0900

    initial commit

---
+++ src/data_structure/unionfind_weighted.cpp
@@ -0,0 +1,44 @@
+/**
+ *  Union-Find(weighted)
+**/
+
+#include <vector>
+
+struct UnionFind {
+  std::vector<int> par, rank, diff_weight;
+ 
+  UnionFind(int n = 1, int SUM_UNITY = 0) {init(n, SUM_UNITY);}
+ 
+  void init(int n = 1, int SUM_UNITY = 0) {
+    par.resize(n); rank.resize(n); diff_weight.resize(n);
+    for (int i = 0; i < n; ++i) {
+      par[i] = i, rank[i] = 0, diff_weight[i] = SUM_UNITY;
+    }
+  }
+ 
+  int root(int x) {
+    if (par[x] == x) return x;
+    else {
+      int r = root(par[x]);
+      diff_weight[x] += diff_weight[par[x]];
+      return par[x] = r;
+    }
+  }
+ 
+  int weight(int x) {root(x); return diff_weight[x];}
+ 
+  bool issame(int x, int y) {return root(x) == root(y);}
+ 
+  void unite(int x, int y, int w) {
+    w += weight(x); w -= weight(y);
+    x = root(x); y = root(y);
+    if (x == y) return;
+    if (rank[x] < rank[y]) std::swap(x, y), w = -w;
+    if (rank[x] == rank[y]) ++rank[x];
+    par[y] = x;
+    diff_weight[y] = w;
+    return;
+  }
+ 
+  int diff(int x, int y) {return weight(y) - weight(x);}
+};
