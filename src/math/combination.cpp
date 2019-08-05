9fcdf2b054d5e3d0c2f918f76c65b5879b56d3ba
kyomukyomupurin <murakami1728@gmail.com>
2019-08-05 16:04:28 +0900

    update

--- src/math/combination.cpp
+++ src/math/combination.cpp
@@ -6,7 +6,7 @@
 
 const int MAX = 2e5;
 const int MOD = 1e9 + 7;
-int64_t fac[MAX] = {}, inv[MAX] = {}, finv[MAX] = {};
+int64 fac[MAX] = {}, inv[MAX] = {}, finv[MAX] = {};
 
 void make_table() {
   fac[0] = 1; fac[1] = 1;
@@ -19,7 +19,7 @@ void make_table() {
   }
 }
 
-int64_t com(int n, int k) {
+int64 com(int n, int k) {
   if (n < k) return 0;
   if (n < 0 || k < 0) return 0;
   return fac[n] * (finv[k] * finv[n - k] % MOD) % MOD;
--- src/math/prime-factorization.cpp
+++ src/math/prime-factorization.cpp
@@ -5,9 +5,9 @@
 #include <iostream>
 #include <vector>
 
-std::vector<std::pair<int64_t, int64_t>> prime_fact(int64_t n){
-  std::vector<std::pair<int64_t, int64_t>> result;
-  for (int64_t i = 2; i * i <= n; ++i){
+std::vector<std::pair<int64, int64>> prime_fact(int64 n){
+  std::vector<std::pair<int64, int64>> result;
+  for (int64 i = 2; i * i <= n; ++i){
     if (n % i != 0) continue;
     int cnt = 0;
     while (n % i == 0){
