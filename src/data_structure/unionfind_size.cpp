192d1b4f36f4b79c0a86459e23a3c6c8776e7ca8
kyomukyomupurin <murakami1728@gmail.com>
2019-09-05 05:45:28 +0900

    add class version

--- src/data_structure/unionfind_size.cpp
+++ src/data_structure/unionfind_size.cpp
@@ -30,3 +30,44 @@ struct UnionFind {
 
   int64 size(int x) {return -par[root(x)];}
 };
+
+// verified by https://atcoder.jp/contests/abc120/tasks/abc120_d
+class UnionFind {
+ public:
+  UnionFind(int n) : n_(n) {}
+  void Initialize();
+  int GetRoot(int x);
+  bool IsSame(int x, int y);
+  void Unite(int x, int y);
+  int GetSize(int x);
+
+ private:
+  const int n_;
+  std::vector<int> parent_;
+};
+
+void UnionFind::Initialize() {
+  parent_.assign(n_, -1);
+}
+
+int UnionFind::GetRoot(int x) {
+  if (parent_[x] < 0) return x;
+  else return parent_[x] = GetRoot(parent_[x]);
+}
+
+bool UnionFind::IsSame(int x, int y) {
+  return GetRoot(x) == GetRoot(y);
+}
+
+void UnionFind::Unite(int x, int y) {
+  x = GetRoot(x); y = GetRoot(y);
+  if (x == y) return;
+  if (parent_[x] > parent_[y]) std::swap(x, y);
+  parent_[x] += parent_[y];
+  parent_[y] = x;
+  return;
+}
+
+int UnionFind::GetSize(int x) {
+  return -parent_[GetRoot(x)];
+}
