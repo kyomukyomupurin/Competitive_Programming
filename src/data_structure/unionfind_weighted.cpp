7ae66a2420c8e45e3c1d04cda55e6bbac265c818
kyomukyomupurin <murakami1728@gmail.com>
2019-09-05 05:00:32 +0900

    add class version

--- src/data_structure/unionfind_weighted.cpp
+++ src/data_structure/unionfind_weighted.cpp
@@ -42,3 +42,61 @@ struct UnionFind {
  
   int diff(int x, int y) {return weight(y) - weight(x);}
 };
+
+class UnionFind {
+ public:
+  UnionFind(int n, int SUM_UNITY = 0) : n_(n), 
+                                        SUM_UNITY_(SUM_UNITY) {}
+  void Initialize();
+  int GetRoot(int x);
+  int Weight(int x);
+  bool IsSame(int x, int y);
+  void Unite(int x, int y, int w);
+
+ private:
+  const int n_;
+  const int SUM_UNITY_;
+  std::vector<int> parent_;
+  std::vector<int> rank_;
+  std::vector<int> diff_weight_;
+};
+
+void UnionFind::Initialize() {
+  parent_.assign(n_, -1);
+  rank_.assign(n_, 0);
+  diff_weight_.assign(n_, SUM_UNITY_);
+}
+
+int UnionFind::GetRoot(int x) {
+  if (parent_[x] == -1) {
+    return x;
+  } else {
+    int r = GetRoot(parent_[x]);
+    diff_weight_[x] += diff_weight_[parent_[x]];
+    return parent_[x] = r;
+  }
+}
+
+int UnionFind::Weight(int x) {
+  GetRoot(x);
+  return diff_weight_[x];
+}
+
+bool UnionFind::IsSame(int x, int y) {
+  return GetRoot(x) == GetRoot(y);
+}
+
+// y is w larger than x
+void UnionFind::Unite(int x, int y, int w) {
+  w += Weight(x); w -= Weight(y);
+  x = GetRoot(x); y = GetRoot(y);
+  if (x == y) return;
+  if (rank_[x] < rank_[y]) {
+    std::swap(x, y);
+    w = -w;
+  }
+  if (rank_[x] == rank_[y]) ++rank_[x];
+  parent_[y] = x;
+  diff_weight_[y] = w;
+  return;
+}
