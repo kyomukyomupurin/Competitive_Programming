4bf8b216ee27f3bc50f0df6a0228aaa388d291f5
kyomukyomupurin <murakami1728@gmail.com>
2019-09-05 08:37:59 +0900

    add class version

--- src/data_structure/segment_tree_rmq.cpp
+++ src/data_structure/segment_tree_rmq.cpp
@@ -38,3 +38,49 @@ struct SegmentTree{
       return std::min(vl, vr);
     }
 };
+
+class SegmentTree {
+ public:
+  SegmentTree(const std::vector<int>& vec) : vec_(vec) {}
+  void Initialize();
+  void Update(int position, int value);
+  int Query(int a, int b, int k, int l, int r);
+
+ private:
+  int n_;
+  const std::vector<int> vec_;
+  std::vector<int> data_;
+};
+
+void SegmentTree::Initialize() {
+  int vsize = static_cast<int>(vec_.size());
+  n_ = 1;
+  while (n_ < vsize) {
+    n_ <<= 1;
+  }
+  data_.assign(2 * n_ - 1, INF);
+  for (int i = 0; i < vsize; ++i) {
+    data_[i + n_ - 1] = vec_[i];
+  }
+  for (int i = n_ - 2; i >= 0; --i) {
+    data_[i] = std::min(data_[2 * i + 1], data_[2 * i + 2]);
+  }
+}
+
+void SegmentTree::Update(int position, int value) {
+  position += n_ - 1;
+  data_[position] = value;
+  while (position > 0){
+    position = (position - 1) / 2;
+    data_[position] = std::min(data_[2 * position + 1], data_[2 * position + 2]);
+  }
+}
+
+int SegmentTree::Query(int a, int b, int k = 0, int l = 0, int r = -1) {
+  if (r < 0) r = n_;
+  if (r <= a || b <= l) return INF;
+  if (a <= l && r <= b) return data_[k];
+  int vl = Query(a, b, 2 * k + 1, l, (l + r) / 2);
+  int vr = Query(a, b, 2 * k + 2, (l + r) / 2, r);
+  return std::min(vl, vr);
+}
