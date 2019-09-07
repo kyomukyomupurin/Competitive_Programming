92a77c3af124108818dc74f2250ea75dc881abce
kyomukyomupurin <murakami1728@gmail.com>
2019-09-07 18:45:40 +0900

    remove struct version

--- src/data_structure/segment_tree_rmq.cc
+++ src/data_structure/segment_tree_rmq.cc
@@ -1,43 +1,10 @@
 /**
- *  SegmentTree(Range Minimum Query)
+ *  Segment Tree(Range Minimum Query)
 **/
 
 #include <vector>
 
 const int INF = (1LL<<31) - 1;
-
-struct SegmentTree{
-  private:
-    int n;
-    std::vector<int> dat;
-  public:
-    SegmentTree(std::vector<int> v) {
-      int SIZE = v.size();
-      n = 1;
-      while (n < SIZE) n <<= 1;
-      dat.resize(2 * n - 1, INF);
-      for (int i = 0; i < SIZE; ++i) dat[i + n - 1] = v[i];
-      for (int i = n - 2; i >= 0; --i) dat[i] = std::min(dat[2 * i + 1], dat[2 * i + 2]);
-    }
-
-    void update(int pos, int val){
-      pos += n - 1;
-      dat[pos] = val;
-      while (pos > 0){
-        pos = (pos - 1) / 2;
-        dat[pos] = std::min(dat[2 * pos + 1], dat[2 * pos + 2]);
-      }
-    }
-
-    int query(int a, int b, int k = 0, int l = 0, int r = -1){
-      if (r < 0) r = n;
-      if (r <= a || b <= l) return INF;
-      if (a <= l && r <= b) return dat[k];
-      int vl = query(a, b, 2 * k + 1, l, (l + r) / 2);
-      int vr = query(a, b, 2 * k + 2, (l + r) / 2, r);
-      return std::min(vl, vr);
-    }
-};
 
 // verified by https://onlinejudge.u-aizu.ac.jp/courses/library/3/DSL/all/DSL_2_A
 class SegmentTree {
--- src/data_structure/segment_tree_rsq.cc
+++ src/data_structure/segment_tree_rsq.cc
@@ -1,39 +1,8 @@
 /**
- *  SegmentTree(Range Sum Query)
+ *  Segment Tree(Range Sum Query)
 **/
 
 #include <vector>
-
-struct SegmentTree{
-  private:
-    int n;
-    std::vector<int> dat;
-  public:
-    SegmentTree(std::vector<int> v) {
-      int SIZE = v.size();
-      n = 1;
-      while (n < SIZE) n <<= 1;
-      dat.resize(2 * n - 1, 0);
-    }
-
-    void add(int pos, int val){
-      pos += n - 1;
-      dat[pos] += val;
-      while (pos > 0){
-        pos = (pos - 1) / 2;
-        dat[pos] = dat[2 * pos + 1] + dat[2 * pos + 2];
-      }
-    }
-
-    int query(int a, int b, int k = 0, int l = 0, int r = -1){
-      if (r < 0) r = n;
-      if (r <= a || b <= l) return 0;
-      if (a <= l && r <= b) return dat[k];
-      int vl = query(a, b, 2 * k + 1, l, (l + r) / 2);
-      int vr = query(a, b, 2 * k + 2, (l + r) / 2, r);
-      return vl + vr;
-    }
-};
 
 // verified by https://onlinejudge.u-aizu.ac.jp/courses/library/3/DSL/all/DSL_2_B
 class SegmentTree {
