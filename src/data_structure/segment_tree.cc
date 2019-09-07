fb487f12521e3a08efbc61e7508b02f227aafe75
kyomukyomupurin <murakami1728@gmail.com>
2019-09-07 20:24:21 +0900

    add non-recursive version

--- src/data_structure/segment_tree.cc
+++ src/data_structure/segment_tree.cc
@@ -48,11 +48,67 @@ void SegmentTree::Update(int position, int new_value) {
   }
 }
 
+// return Query[a, b)
 int SegmentTree::Query(int a, int b, int k = 0, int l = 0, int r = -1) {
   if (r < 0) r = n_;
   if (r <= a || b <= l) return initial_value_;
   if (a <= l && r <= b) return data_[k];
   int vl = Query(a, b, 2 * k + 1, l, (l + r) / 2);
   int vr = Query(a, b, 2 * k + 2, (l + r) / 2, r);
+  return operation_(vl, vr);
+}
+
+// non-recursive version
+
+class SegmentTree2 {
+ public:
+  SegmentTree2(const std::vector<int>& vec, const int& initial_value,
+              std::function<int(int, int)> operation)
+        : initial_value_(initial_value),
+          vec_(vec),
+          operation_(operation) {}
+  void Initialize();
+  void Update(int position, int new_value);
+  int Query(int a, int b);
+
+ private:
+  int n_;
+  const int initial_value_;
+  const std::vector<int> vec_;
+  std::vector<int> data_;
+  std::function<int(int, int)> operation_;
+};
+
+void SegmentTree2::Initialize() {
+  int vsize = static_cast<int>(vec_.size());
+  n_ = 1;
+  while (n_ < vsize) {
+    n_ <<= 1;
+  }
+  data_.assign(2 * n_, initial_value_);
+  for (int i = 0; i < vsize; ++i) {
+    data_[i + n_] = vec_[i];
+  }
+  for (int i = n_ - 1; i >= 0; --i) {
+    data_[i] = operation_(data_[2 * i], data_[2 * i + 1]);
+  }
+}
+
+void SegmentTree2::Update(int position, int new_value) {
+  position += n_;
+  data_[position] = new_value;
+  while (position > 0){
+    position >>= 1;
+    data_[position] = operation_(data_[2 * position], data_[2 * position + 1]);
+  }
+}
+
+// return Query[a, b)
+int SegmentTree2::Query(int a, int b) {
+  int vl = initial_value_, vr = initial_value_;
+  for(a += n_, b += n_; a < b; a >>= 1, b >>= 1) {
+    if(a & 1) vl = operation_(vl, data_[a++]);
+    if(b & 1) vr = operation_(data_[--b], vr);
+  }
   return operation_(vl, vr);
 }
