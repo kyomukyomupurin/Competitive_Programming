39dfecc84b4fe874fa9cfd805aa65862ce1b8412
kyomukyomupurin <murakami1728@gmail.com>
2019-09-13 14:25:04 +0900

    initialize in constructor

--- src/etc/xorshift.cc
+++ src/etc/xorshift.cc
@@ -10,17 +10,22 @@
 // verified by https://atcoder.jp/contests/abc139/tasks/abc139_f
 class Xorshift128plus {
  public:
+  Xorshift128plus() { Initialize(); }
   using result_type = uint64_t;
-  static constexpr uint64_t min() { return std::numeric_limits<result_type>::min(); }
-  static constexpr uint64_t max() { return std::numeric_limits<result_type>::max(); }
+  static constexpr uint64_t min() {
+    return std::numeric_limits<result_type>::min();
+  }
+  static constexpr uint64_t max() {
+    return std::numeric_limits<result_type>::max();
+  }
   uint64_t operator()() { return GetNext(); }
-  void Initialize();
 
  private:
   static uint64_t result_;
   static uint64_t seed0_;
   static uint64_t seed1_;
   void Next();
+  void Initialize();
   uint64_t GetNext();
 };
 
@@ -30,13 +35,13 @@ uint64_t Xorshift128plus::seed0_ = static_cast<uint64_t>(
 uint64_t Xorshift128plus::seed1_ = static_cast<uint64_t>(
     std::chrono::steady_clock::now().time_since_epoch().count());
 
-inline void Xorshift128plus::Initialize() {
+void Xorshift128plus::Initialize() {
   for (int i = 0; i < 40; ++i) {
     Next();
   }
 }
 
-inline void Xorshift128plus::Next() {
+void Xorshift128plus::Next() {
   uint64_t s1 = seed0_;
   uint64_t s0 = seed1_;
   result_ = s0 + s1;
@@ -45,7 +50,7 @@ inline void Xorshift128plus::Next() {
   seed1_ = s1 ^ s0 ^ (s1 >> 18) ^ (s0 >> 5);
 }
 
-inline uint64_t Xorshift128plus::GetNext() {
+uint64_t Xorshift128plus::GetNext() {
   Next();
   return result_;
 }
