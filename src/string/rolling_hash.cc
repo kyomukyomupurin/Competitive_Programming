fd9eef38aa2d2c82708c71a629703cfbf8adfe00
kyomukyomupurin <murakami1728@gmail.com>
2019-09-21 16:08:55 +0900

    update

--- src/string/rolling_hash.cc
+++ src/string/rolling_hash.cc
@@ -5,6 +5,9 @@
 #include <chrono>
 #include <string>
 #include <vector>
+#include <ext/random>
+
+using ull = unsigned long long;
 
 // verified by
 // https://onlinejudge.u-aizu.ac.jp/courses/lesson/1/ALDS1/all/ALDS1_14_B
@@ -12,89 +15,52 @@
 class RollingHash {
  public:
   RollingHash(const std::string& s) : s_(s) { Initialize(); }
-  std::pair<unsigned int, unsigned int> GetHash(int left, int right);
+  std::pair<ull, ull> GetHash(int left, int right);
 
  private:
   const std::string s_;
-  static std::vector<unsigned int> bases_;
-  static unsigned int base0_;
-  static unsigned int base1_;
-  std::vector<unsigned int> hash0_;
-  std::vector<unsigned int> hash1_;
-  std::vector<unsigned int> power0_;
-  std::vector<unsigned int> power1_;
-  static constexpr unsigned int mod0_ = 2020202077;
-  static constexpr unsigned int mod1_ = 2020202111;
+  static ull base0_;
+  static ull base1_;
+  static constexpr ull mod0_ = 2020202077;
+  static constexpr ull mod1_ = 2020202111;
+  std::vector<ull> hash0_;
+  std::vector<ull> hash1_;
+  static std::vector<ull> power0_;
+  static std::vector<ull> power1_;
+  static __gnu_cxx::sfmt19937_64 sfmt_;
   void Initialize();
-  unsigned int mul0(unsigned int a, unsigned int b);
-  unsigned int mul1(unsigned int a, unsigned int b);
 };
 
-std::vector<unsigned int> RollingHash::bases_ = {
-    10007, 10009, 10037, 10039, 10061, 10067, 10069, 10079, 10091, 10093, 10099,
-    10103, 10111, 10133, 10139, 10141, 10151, 10159, 10163, 10169, 10177, 10181,
-    10193, 10211, 10223, 10243, 10247, 10253, 10259, 10267, 10271, 10273, 10289,
-    10301, 10303, 10313, 10321, 10331, 10333, 10337, 10343, 10357, 10369, 10391,
-    10399, 10427, 10429, 10433, 10453, 10457, 10459, 10463, 10477, 10487, 10499,
-    10501, 10513, 10529, 10531, 10559, 10567, 10589, 10597, 10601, 10607, 10613,
-    10627, 10631, 10639, 10651, 10657, 10663, 10667, 10687, 10691, 10709, 10711,
-    10723, 10729, 10733, 10739, 10753, 10771, 10781, 10789, 10799, 10831, 10837,
-    10847, 10853, 10859, 10861, 10867, 10883, 10889, 10891, 10903, 10909, 10937,
-    10939, 10949, 10957, 10973, 10979, 10987, 10993, 11003, 11027, 11047, 11057,
-    11059, 11069, 11071, 11083, 11087, 11093, 11113, 11117, 11119, 11131, 11149,
-    11159, 11161, 11171, 11173, 11177, 11197};
+void RollingHash::Initialize() {
+  size_t n = s_.size();
+  hash0_.assign(n + 1, 0);
+  hash1_.assign(n + 1, 0);
+  for (size_t i = 0; i < n; ++i) {
+    hash0_[i + 1] = (hash0_[i] * base0_ + s_[i]) % mod0_;
+    hash1_[i + 1] = (hash1_[i] * base1_ + s_[i]) % mod1_;
+  }
+  while (power0_.size() <= n) {
+    power0_.emplace_back(power0_.back() * base0_ % mod0_);
+    power1_.emplace_back(power1_.back() * base1_ % mod1_);
+  }
+}
 
-unsigned int RollingHash::base0_ =
-    bases_[std::chrono::steady_clock::now().time_since_epoch().count() & 127];
-
-unsigned int RollingHash::base1_ =
-    bases_[std::chrono::steady_clock::now().time_since_epoch().count() & 127];
-
-// return hash pair of [left, right)
-inline std::pair<unsigned int, unsigned int> RollingHash::GetHash(int left,
-                                                                  int right) {
-  unsigned int ret0 =
-      hash0_[right] + mod0_ - mul0(hash0_[left], power0_[right - left]);
+inline std::pair<ull, ull> RollingHash::GetHash(int left, int right) {
+  ull ret0 =
+      hash0_[right] + mod0_ - hash0_[left] * power0_[right - left] % mod0_;
   if (ret0 >= mod0_) ret0 -= mod0_;
-  unsigned int ret1 =
-      hash1_[right] + mod1_ - mul1(hash1_[left], power1_[right - left]);
+  ull ret1 =
+      hash1_[right] + mod1_ - hash1_[left] * power1_[right - left] % mod1_;
   if (ret1 >= mod1_) ret1 -= mod1_;
 
   return {ret0, ret1};
 }
 
-void RollingHash::Initialize() {
-  int ssize = static_cast<int>(s_.size());
-  hash0_.assign(ssize + 1, 0);
-  hash1_.assign(ssize + 1, 0);
-  power0_.assign(ssize + 1, 0);
-  power1_.assign(ssize + 1, 0);
-  power0_[0] = 1;
-  power1_[0] = 1;
-  for (int i = 0; i < ssize; ++i) {
-    hash0_[i + 1] = mul0(hash0_[i], base0_) + s_[i];
-    hash1_[i + 1] = mul1(hash1_[i], base1_) + s_[i];
-    power0_[i + 1] = mul0(power0_[i], base0_);
-    power1_[i + 1] = mul1(power1_[i], base1_);
-    if (hash0_[i + 1] >= mod0_) hash0_[i + 1] -= mod0_;
-    if (hash1_[i + 1] >= mod1_) hash1_[i + 1] -= mod1_;
-  }
-}
-
-inline unsigned int RollingHash::mul0(unsigned int a, unsigned int b) {
-  unsigned long long x =
-      static_cast<unsigned long long>(a) * static_cast<unsigned long long>(b);
-  unsigned int xh = static_cast<unsigned int>(x >> 32),
-               xl = static_cast<unsigned int>(x), d, m;
-  asm("divl %4; \n\t" : "=a"(d), "=d"(m) : "d"(xh), "a"(xl), "r"(mod0_));
-  return m;
-}
-
-inline unsigned int RollingHash::mul1(unsigned int a, unsigned int b) {
-  unsigned long long x =
-      static_cast<unsigned long long>(a) * static_cast<unsigned long long>(b);
-  unsigned int xh = static_cast<unsigned int>(x >> 32),
-               xl = static_cast<unsigned int>(x), d, m;
-  asm("divl %4; \n\t" : "=a"(d), "=d"(m) : "d"(xh), "a"(xl), "r"(mod1_));
-  return m;
-}
+__gnu_cxx::sfmt19937_64 RollingHash::sfmt_(
+    std::chrono::steady_clock::now().time_since_epoch().count());
+ull RollingHash::base0_ = std::uniform_int_distribution<ull>(
+    2, RollingHash::mod0_)(RollingHash::sfmt_);
+ull RollingHash::base1_ = std::uniform_int_distribution<ull>(
+    2, RollingHash::mod1_)(RollingHash::sfmt_);
+std::vector<ull> RollingHash::power0_{1};
+std::vector<ull> RollingHash::power1_{1};
