e255d3f8550b80f3742ebdc75ce09f7a40d24cd0
kyomukyomupurin <murakami1728@gmail.com>
2019-10-01 18:41:25 +0900

    initial commit

---
+++ src/typical/dfs.cc
@@ -0,0 +1,19 @@
+/**
+ *  dfs on graph
+**/
+
+#include <vector>
+
+std::vector<std::vector<int>> graph(202020);
+
+auto dfs = [&](const auto& dfs, int cur, int prev) -> void {
+  for (int next : graph[cur]) {
+    if (next == prev) continue;
+    // do something
+    dfs(dfs, next, cur);
+  }
+};
+
+/*
+dfs(dfs, 0, -1);
+*/
