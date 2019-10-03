da01858107e5784731aaf168472dd0b5a4addabf
kyomukyomupurin <murakami1728@gmail.com>
2019-10-04 05:28:29 +0900

    update

--- src/etc/timer.cc
+++ src/etc/timer.cc
@@ -7,17 +7,17 @@
 // verified by https://atcoder.jp/contests/abc139/tasks/abc139_f
 class Timer {
  public:
+  Timer()
+      : start_(std::chrono::system_clock::now()),
+        stop_(std::chrono::system_clock::now()) {}
   void Start();
   void Stop();
   double GetElapsedTime();
 
  private:
-  static std::chrono::system_clock::time_point start_;
-  static std::chrono::system_clock::time_point stop_;
+  std::chrono::system_clock::time_point start_;
+  std::chrono::system_clock::time_point stop_;
 };
-
-std::chrono::system_clock::time_point Timer::start_;
-std::chrono::system_clock::time_point Timer::stop_;
 
 void Timer::Start() { start_ = std::chrono::system_clock::now(); }
 
