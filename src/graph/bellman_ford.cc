1be1eee9f203d68df2bbafe816ad9a1e0fba9f87
kyomukyomupurin <murakami1728@gmail.com>
2019-10-03 18:14:33 +0900

    format

--- src/graph/bellman_ford.cc
+++ src/graph/bellman_ford.cc
@@ -6,7 +6,7 @@
 #include <vector>
 
 // verified by https://onlinejudge.u-aizu.ac.jp/courses/library/5/GRL/1/GRL_1_B
-template<typename T>
+template <typename T>
 class BellmanFord {
  public:
   BellmanFord(const int& node_size) : node_size_(node_size) { Initialize(); }
@@ -25,26 +25,26 @@ class BellmanFord {
   void Initialize();
 };
 
-template<typename T>
+template <typename T>
 void BellmanFord<T>::AddEdge(const int& from, const int& to, const T& cost) {
   es.push_back({from, to, cost});
 }
 
 // use after bf.solve(source) !
-template<typename T>
+template <typename T>
 bool BellmanFord<T>::NegativeCycle() {
-  for (const auto &e : es) {
+  for (const auto& e : es) {
     if (distance_[e.from] == std::numeric_limits<T>::max() / 2) continue;
     if (distance_[e.from] + e.cost < distance_[e.to]) return true;
   }
   return false;
 }
 
-template<typename T>
+template <typename T>
 void BellmanFord<T>::Solve(const int& source) {
   distance_[source] = 0;
   for (int i = 0; i < node_size_ - 1; ++i) {
-    for (const auto &e : es) {
+    for (const auto& e : es) {
       if (distance_[e.from] == std::numeric_limits<T>::max() / 2) continue;
       if (distance_[e.to] > distance_[e.from] + e.cost) {
         distance_[e.to] = distance_[e.from] + e.cost;
@@ -53,7 +53,7 @@ void BellmanFord<T>::Solve(const int& source) {
   }
 }
 
-template<typename T>
+template <typename T>
 void BellmanFord<T>::Initialize() {
   distance_.assign(node_size_, std::numeric_limits<T>::max() / 2);
 }
