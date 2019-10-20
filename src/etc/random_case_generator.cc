e1653893108c281a5e9c1c580c5f268fa1bd44ee
kyomukyomupurin <murakami1728@gmail.com>
2019-10-20 10:35:33 +0900

    update

--- src/etc/random_case_generator.cc
+++ src/etc/random_case_generator.cc
@@ -7,122 +7,65 @@
 // v_0 v_1 v_2 ... v_n - 1
 
 #include <chrono>
-#include <iostream>
+#include <ext/random>
 #include <fstream>
-#include <random>
+#include <iostream>
 #include <vector>
 
-class Xorshift128plus {
+class RandomCase {
  public:
-  Xorshift128plus() { Initialize(); }
-  using result_type = uint64_t;
-  static constexpr uint64_t min() {
-    return std::numeric_limits<result_type>::min();
-  }
-  static constexpr uint64_t max() {
-    return std::numeric_limits<result_type>::max();
-  }
-  uint64_t operator()() { return GetNext(); }
+  RandomCase(double run_time) : run_time_(run_time) {}
+  void Run();
 
  private:
-  static uint64_t result_;
-  static uint64_t seed0_;
-  static uint64_t seed1_;
-  void Next();
-  void Initialize();
-  uint64_t GetNext();
+  double run_time_;
+  void Generate();
+  int Solve1();
+  int Solve2();
 };
 
-uint64_t Xorshift128plus::result_;
-uint64_t Xorshift128plus::seed0_ = static_cast<uint64_t>(
-    std::chrono::steady_clock::now().time_since_epoch().count());
-uint64_t Xorshift128plus::seed1_ = static_cast<uint64_t>(
-    std::chrono::steady_clock::now().time_since_epoch().count());
-
-void Xorshift128plus::Initialize() {
-  for (int i = 0; i < 40; ++i) {
-    Next();
-  }
-}
-
-void Xorshift128plus::Next() {
-  uint64_t s1 = seed0_;
-  uint64_t s0 = seed1_;
-  result_ = s0 + s1;
-  seed0_ = s0;
-  s1 ^= s1 << 23;
-  seed1_ = s1 ^ s0 ^ (s1 >> 18) ^ (s0 >> 5);
-}
-
-uint64_t Xorshift128plus::GetNext() {
-  Next();
-  return result_;
-}
-
-class Timer {
- public:
-  Timer()
-      : start_(std::chrono::system_clock::now()),
-        stop_(std::chrono::system_clock::now()) {}
-  void Start();
-  void Stop();
-  double GetElapsedTime();
-
- private:
-  std::chrono::system_clock::time_point start_;
-  std::chrono::system_clock::time_point stop_;
-};
-
-void Timer::Start() { start_ = std::chrono::system_clock::now(); }
-
-void Timer::Stop() { stop_ = std::chrono::system_clock::now(); }
-
-double Timer::GetElapsedTime() {
-  return std::chrono::duration_cast<std::chrono::milliseconds>(stop_ - start_)
-      .count();
-}
-
-void generate_random_case() {
-  std::ofstream outputfile("random_case.txt");
-  Xorshift128plus xor128;
-  std::uniform_int_distribution<int> n(1, 100);
-  std::uniform_int_distribution<int> vec(1, 1000);
-  int num = n(xor128);
-  outputfile << num << '\n';
-  for (int i = 0; i < num; ++i) {
-    outputfile << vec(xor128) << " ";
-  }
-  outputfile.close();
-}
-
-int solve1() {
-  // naive solution
-  // it must be 100% correct!
-}
-
-int solve2() {
-  // solution
-}
-
-void execute() {
-  Timer t;
-  t.Start();
-
-  while (t.GetElapsedTime() < 10000) {
-    generate_random_case();
-
+void RandomCase::Run() {
+  std::chrono::system_clock::time_point start =
+      std::chrono::system_clock::now();
+  std::chrono::system_clock::time_point stop = std::chrono::system_clock::now();
+  while (std::chrono::duration_cast<std::chrono::milliseconds>(stop - start)
+             .count() < run_time_) {
+    Generate();
     std::ifstream in("random_case.txt");
     std::cin.rdbuf(in.rdbuf());
-    int ans1 = solve1();
+    int ans1 = Solve1();
 
     std::ifstream in2("random_case.txt");
     std::cin.rdbuf(in2.rdbuf());
-    int ans2 = solve2();
+    int ans2 = Solve2();
 
     if (ans1 != ans2) {
       std::cout << "found!" << '\n';
       break;
     }
-    t.Stop();
+    stop = std::chrono::system_clock::now();
   }
+}
+
+void RandomCase::Generate() {
+  std::ofstream outputfile("random_case.txt");
+  __gnu_cxx::sfmt19937_64 sfmt(
+      std::chrono::steady_clock::now().time_since_epoch().count());
+  std::uniform_int_distribution<int> n(1, 100);
+  std::uniform_int_distribution<int> vec(1, 1000);
+  int num = n(sfmt);
+  outputfile << num << '\n';
+  for (int i = 0; i < num; ++i) {
+    outputfile << vec(sfmt) << " ";
+  }
+  outputfile.close();
+}
+
+int RandomCase::Solve1() {
+  // naive solution
+  // it must be 200% correct!
+}
+
+int RandomCase::Solve2() {
+  // your solution
 }
