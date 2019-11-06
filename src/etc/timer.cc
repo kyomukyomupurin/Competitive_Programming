ac376d19e1157cd8bbf65a3ce0cb7d818bf55b67
kyomukyomupurin <murakami1728@gmail.com>
2019-11-07 03:13:35 +0900

    inline

--- src/etc/timer.cc
+++ src/etc/timer.cc
@@ -7,24 +7,18 @@
 // verified by https://atcoder.jp/contests/abc139/tasks/abc139_f
 class Timer {
  public:
-  double GetElapsedTime();
-  void Start();
+  double GetElapsedTime() {
+    return static_cast<double>(GetCycle() - start_) /
+           static_cast<double>(clock_frequency_);
+  }
+  void Start() { start_ = GetCycle(); }
 
  private:
   int64_t start_;
   static constexpr int64_t clock_frequency_ = 2800000000;
-  int64_t GetCycle();
+  int64_t GetCycle() {
+    uint32_t low, high;
+    __asm__ volatile("rdtsc" : "=a"(low), "=d"(high));
+    return (static_cast<int64_t>(low)) | (static_cast<int64_t>(high) << 32);
+  }
 };
-
-double Timer::GetElapsedTime() {
-  return static_cast<double>(GetCycle() - start_) /
-         static_cast<double>(clock_frequency_);
-}
-
-void Timer::Start() { start_ = GetCycle(); }
-
-int64_t Timer::GetCycle() {
-  uint32_t low, high;
-  __asm__ volatile("rdtsc" : "=a"(low), "=d"(high));
-  return (static_cast<int64_t>(low)) | (static_cast<int64_t>(high) << 32);
-}
