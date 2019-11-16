22ee82eba3165ea71d1a9221fa54d38119ed01d7
kyomukyomupurin <murakami1728@gmail.com>
2019-11-17 01:53:19 +0900

    update

--- src/etc/template.cc
+++ src/etc/template.cc
@@ -30,17 +30,8 @@ using int64 = long long;
 #define debug(_) cerr << #_ << ": " << (_) << '\n'
 template<typename T> inline void chmin(T &a, T b) { if (a > b) a = b; return; }
 template<typename T> inline void chmax(T &a, T b) { if (a < b) a = b; return; }
-template<typename T> ostream& operator<<(ostream& os, const vector<T>& vec) {
-  os << '{'; for (size_t i = 0; i < vec.size(); ++i) { os << vec[i]; if (i != vec.size() - 1) os << ','; } os << '}';
-  return os;
-}
-template<typename T, typename U> ostream& operator<<(ostream& os, const pair<T, U>& p) {
-  return os << '{' << p.first << " " << p.second << '}';
-}
-constexpr int INF = numeric_limits<int>::max() / 2;
-constexpr int64 INF64 = numeric_limits<int64>::max() / 2;
-constexpr int MOD = 1000000007;
-const double PI = acos(-1);
+template<typename T> ostream& operator<<(ostream& os, const vector<T>& vec) { os << '{'; for (size_t i = 0; i < vec.size(); ++i) { os << vec[i]; if (i != vec.size() - 1) os << ','; } os << '}'; return os; }
+template<typename T, typename U> ostream& operator<<(ostream& os, const pair<T, U>& p) { return os << '{' << p.first << " " << p.second << '}'; }
 
 int main() {
   ios_base::sync_with_stdio(false);
