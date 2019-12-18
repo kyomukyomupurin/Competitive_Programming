6c8bfe4610396d9080c52a8cf1af7d06dd36c015
kyomukyomupurin <murakami1728@gmail.com>
2019-12-18 21:41:24 +0900

    inline implementation

--- src/algorithm/2D_cumulative_sum.cc
+++ src/algorithm/2D_cumulative_sum.cc
@@ -13,40 +13,34 @@ class CumulativeSum2D {
       : vec_(vec), height_(vec.size()), width_(vec[0].size()) {
     Build();
   }
-  T Get(int sx, int sy, int gx, int gy);
+  // return sum of [sx, gx] * [sy, gy]
+  T Get(int sx, int sy, int gx, int gy) {
+    assert(sx <= gx && sy <= gy);
+    assert(0 <= sx && sx < height_ && 0 <= gx && gx < height_ && 0 <= sy &&
+          sy < width_ && 0 <= gy && gy < width_);
+    return sum_[gx + 1][gy + 1] - sum_[gx + 1][sy] - sum_[sx][gy + 1] +
+          sum_[sx][sy];
+  }
 
  private:
   size_t height_;
   size_t width_;
   std::vector<vector<T>> vec_;
   std::vector<vector<T>> sum_;
-  void Build();
+  void Build() {
+    sum_.assign(height_ + 1, vector<T>(width_ + 1, static_cast<T>(0)));
+    for (size_t i = 0; i < height_; ++i) {
+      for (size_t j = 0; j < width_; ++j) {
+        sum_[i + 1][j + 1] = sum_[i + 1][j] + vec_[i][j];
+      }
+    }
+    for (size_t i = 0; i < width_; ++i) {
+      for (size_t j = 0; j < height_; ++j) {
+        sum_[j + 1][i + 1] += sum_[j][i + 1];
+      }
+    }
+  }
 };
-
-// return sum of [sx, gx] * [sy, gy]
-template <typename T>
-T CumulativeSum2D<T>::Get(int sx, int sy, int gx, int gy) {
-  assert(sx <= gx && sy <= gy);
-  assert(0 <= sx && sx < height_ && 0 <= gx && gx < height_ && 0 <= sy &&
-         sy < width_ && 0 <= gy && gy < width_);
-  return sum_[gx + 1][gy + 1] - sum_[gx + 1][sy] - sum_[sx][gy + 1] +
-         sum_[sx][sy];
-}
-
-template <typename T>
-void CumulativeSum2D<T>::Build() {
-  sum_.assign(height_ + 1, vector<T>(width_ + 1, static_cast<T>(0)));
-  for (size_t i = 0; i < height_; ++i) {
-    for (size_t j = 0; j < width_; ++j) {
-      sum_[i + 1][j + 1] = sum_[i + 1][j] + vec_[i][j];
-    }
-  }
-  for (size_t i = 0; i < width_; ++i) {
-    for (size_t j = 0; j < height_; ++j) {
-      sum_[j + 1][i + 1] += sum_[j][i + 1];
-    }
-  }
-}
 
 /*
 void ABC106_D() {
@@ -63,6 +57,6 @@ void ABC106_D() {
     int p, q; cin >> p >> q;
     --p; --q;
     cout << cs2.Get(p, p, q, q) << '\n';
-  }  
+  }
 }
 */
