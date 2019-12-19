be336d91aa4905d16c3e0036b438618bd5c9c2f9
kyomukyomupurin <murakami1728@gmail.com>
2019-12-19 20:52:01 +0900

    inline implementation

--- src/string/rolling_hash.cc
+++ src/string/rolling_hash.cc
@@ -5,7 +5,7 @@
 #include <chrono>
 #include <string>
 #include <vector>
-#include <ext/random>
+#include <random>
 
 using ull = unsigned long long;
 
@@ -15,7 +15,15 @@ using ull = unsigned long long;
 class RollingHash {
  public:
   RollingHash(const std::string& s) : s_(s) { Initialize(); }
-  std::pair<ull, ull> GetHash(int left, int right);
+  std::pair<ull, ull> GetHash(int left, int right) {
+    ull ret0 =
+        (hash0_[right] - hash0_[left] * power0_[right - left] % mod0_ + mod0_) %
+        mod0_;
+    ull ret1 =
+        (hash1_[right] - hash1_[left] * power1_[right - left] % mod1_ + mod1_) %
+        mod1_;
+    return {ret0, ret1};
+  }
 
  private:
   const std::string s_;
@@ -27,40 +35,28 @@ class RollingHash {
   std::vector<ull> hash1_;
   static std::vector<ull> power0_;
   static std::vector<ull> power1_;
-  static __gnu_cxx::sfmt19937_64 sfmt_;
-  void Initialize();
+  static std::mt19937_64 mt_;
+  void Initialize() {
+    size_t n = s_.size();
+    hash0_.assign(n + 1, 0);
+    hash1_.assign(n + 1, 0);
+    for (size_t i = 0; i < n; ++i) {
+      hash0_[i + 1] = (hash0_[i] * base0_ + s_[i]) % mod0_;
+      hash1_[i + 1] = (hash1_[i] * base1_ + s_[i]) % mod1_;
+    }
+    while (power0_.size() <= n) {
+      power0_.emplace_back(power0_.back() * base0_ % mod0_);
+      power1_.emplace_back(power1_.back() * base1_ % mod1_);
+    }
+  }
 };
 
-void RollingHash::Initialize() {
-  size_t n = s_.size();
-  hash0_.assign(n + 1, 0);
-  hash1_.assign(n + 1, 0);
-  for (size_t i = 0; i < n; ++i) {
-    hash0_[i + 1] = (hash0_[i] * base0_ + s_[i]) % mod0_;
-    hash1_[i + 1] = (hash1_[i] * base1_ + s_[i]) % mod1_;
-  }
-  while (power0_.size() <= n) {
-    power0_.emplace_back(power0_.back() * base0_ % mod0_);
-    power1_.emplace_back(power1_.back() * base1_ % mod1_);
-  }
-}
-
-inline std::pair<ull, ull> RollingHash::GetHash(int left, int right) {
-  ull ret0 =
-      (hash0_[right] - hash0_[left] * power0_[right - left] % mod0_ + mod0_) %
-      mod0_;
-  ull ret1 =
-      (hash1_[right] - hash1_[left] * power1_[right - left] % mod1_ + mod1_) %
-      mod1_;
-  return {ret0, ret1};
-}
-
-__gnu_cxx::sfmt19937_64 RollingHash::sfmt_(
+std::mt19937_64 RollingHash::mt_(
     std::chrono::steady_clock::now().time_since_epoch().count());
-ull RollingHash::base0_ = std::uniform_int_distribution<ull>(
-    2, RollingHash::mod0_)(RollingHash::sfmt_);
-ull RollingHash::base1_ = std::uniform_int_distribution<ull>(
-    2, RollingHash::mod1_)(RollingHash::sfmt_);
+ull RollingHash::base0_ =
+    std::uniform_int_distribution<ull>(2, RollingHash::mod0_)(RollingHash::mt_);
+ull RollingHash::base1_ =
+    std::uniform_int_distribution<ull>(2, RollingHash::mod1_)(RollingHash::mt_);
 std::vector<ull> RollingHash::power0_{1};
 std::vector<ull> RollingHash::power1_{1};
 
