91e3d7b57f078ac3d5cbbf3a24d4cd47351e436e
kyomukyomupurin <murakami1728@gmail.com>
2019-12-19 21:19:53 +0900

    inline implementation and typename -> class

--- src/graph/tree_diameter.cc
+++ src/graph/tree_diameter.cc
@@ -5,12 +5,15 @@
 #include <vector>
 
 // verified by https://onlinejudge.u-aizu.ac.jp/courses/library/5/GRL/5/GRL_5_A
-template <typename T>
+template <class T>
 class TreeDiameter {
  public:
   TreeDiameter(int node_size) : node_size_(node_size) { Initialize(); }
-  void AddEdge(int from, int to, T cost);
-  T GetDiameter();
+  void AddEdge(int from, int to, T cost) {
+    graph_[from].push_back({cost, to});
+    graph_[to].push_back({cost, from});
+  }
+  T GetDiameter() { return BFS(BFS(0).second).first; }
 
  private:
   struct edge {
@@ -19,47 +22,28 @@ class TreeDiameter {
   };
   int node_size_;
   std::vector<std::vector<edge>> graph_;
-  void Initialize();
-  std::pair<T, int> BFS(int source);
-};
-
-template <typename T>
-void TreeDiameter<T>::Initialize() {
-  graph_.resize(node_size_);
-}
-
-template <typename T>
-void TreeDiameter<T>::AddEdge(int from, int to, T cost) {
-  graph_[from].push_back({cost, to});
-  graph_[to].push_back({cost, from});
-}
-
-template <typename T>
-T TreeDiameter<T>::GetDiameter() {
-  return BFS(BFS(0).second).first;
-}
-
-template <typename T>
-std::pair<T, int> TreeDiameter<T>::BFS(int source) {
-  std::pair<T, int> result = {static_cast<T>(0), 0};
-  std::queue<int> q;
-  bool visited[node_size_] = {};
-  vector<T> dist(node_size_, 0);
-  visited[source] = true;
-  q.push(source);
-  while (!q.empty()) {
-    int top = q.front();
-    q.pop();
-    for (const auto& next : graph_[top]) {
-      if (visited[next.to]) continue;
-      visited[next.to] = true;
-      dist[next.to] += dist[top] + next.cost;
-      q.push(next.to);
-      result = max(result, {dist[next.to], next.to});
+  void Initialize() { graph_.resize(node_size_); }
+  std::pair<T, int> BFS(int source) {
+    std::pair<T, int> result = {static_cast<T>(0), 0};
+    std::queue<int> q;
+    bool visited[node_size_] = {};
+    vector<T> dist(node_size_, 0);
+    visited[source] = true;
+    q.push(source);
+    while (!q.empty()) {
+      int top = q.front();
+      q.pop();
+      for (const auto& next : graph_[top]) {
+        if (visited[next.to]) continue;
+        visited[next.to] = true;
+        dist[next.to] += dist[top] + next.cost;
+        q.push(next.to);
+        result = max(result, {dist[next.to], next.to});
+      }
     }
+    return result;
   }
-  return result;
-}
+};
 
 /*
 void GRL_5_A() {
