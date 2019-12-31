a4362b51e64b3da4b5a907b8a7cac3b3693ff015
kyomukyomupurin <murakami1728@gmail.com>
2019-12-31 15:49:12 +0900

    add range add query

--- src/data_structure/fenwick_tree.cc
+++ src/data_structure/fenwick_tree.cc
@@ -1,10 +1,12 @@
 /**
- *  Fenwick Tree(binary indexed tree) (for point add query and range sum query)
+ *  Fenwick Tree(binary indexed tree) (for point add query and range sum query, range add query and range sum query)
 **/
 
 #include <vector>
 
-// verified by https://onlinejudge.u-aizu.ac.jp/courses/library/3/DSL/2/DSL_2_B
+// verified by 
+// https://onlinejudge.u-aizu.ac.jp/courses/library/3/DSL/2/DSL_2_B
+// https://onlinejudge.u-aizu.ac.jp/courses/library/3/DSL/2/DSL_2_G
 template <class T>
 class FenwickTree {
  public:
@@ -32,6 +34,37 @@ class FenwickTree {
   T GetRangeSum(int left, int right) {
     return GetSum(right) - GetSum(left - 1);
   }
+  // for range add query
+  class FenwickTreeRAQ {
+   public:
+    FenwickTreeRAQ(const std::vector<T>& data)
+        : n_(data.size()),
+          ft1_(FenwickTree<T>(data)),
+          ft2_(FenwickTree<T>(data)) {}
+    // add range [left, right)
+    void RangeAdd(int left, int right, T value) {
+      Add(ft1_, left, right, value);
+      Add(ft2_, left, right, -value * static_cast<T>(left - 1));
+      Add(ft2_, right, n_, value * static_cast<T>(right - left));
+    }
+    // get sum of [0, i]
+    T GetSum(size_t pos) {
+      return ft1_.GetSum(pos) * static_cast<T>(pos) + ft2_.GetSum(pos);
+    }
+    // get sum of [left, right]
+    T GetRangeSum(int left, int right) {
+      return GetSum(right) - GetSum(left - 1);
+    }
+
+   private:
+    size_t n_;
+    FenwickTree<T> ft1_;
+    FenwickTree<T> ft2_;
+    void Add(FenwickTree<T>& ft, int left, int right, T value) {
+      ft.Add(left, value);
+      ft.Add(right, -value);
+    }
+  };
 
  private:
   size_t n_;
@@ -53,6 +86,23 @@ void DSL_2_B() {
       ft.Add(x - 1, y);
     } else {
       cout << ft.GetRangeSum(x - 1, y - 1) << '\n';
+    }
+  }
+}
+*/
+
+/*
+void DSL_2_G() {
+  int n, q; cin >> n >> q;
+  FenwickTree<int64>::FenwickTreeRAQ ft(vector<int64>(n, 0));
+  for (int i = 0; i < q; ++i) {
+    int com; cin >> com;
+    if (com == 0) {
+      int s, t, x; cin >> s >> t >> x;
+      ft.RangeAdd(s - 1, t, x);
+    } else {
+      int s, t; cin >> s >> t;
+      cout << ft.GetRangeSum(s - 1, t - 1) << '\n';
     }
   }
 }
