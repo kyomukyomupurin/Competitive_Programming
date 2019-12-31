4316beec8183e151457383592ec31e40950cbd2e
kyomukyomupurin <murakami1728@gmail.com>
2019-12-31 16:02:02 +0900

    update

--- src/data_structure/lazy_segment_tree_ruqrmq.cc
+++ src/data_structure/lazy_segment_tree_ruqrmq.cc
@@ -5,6 +5,7 @@
 #include <vector>
 
 // not fot raange sum query!!
+// verified by https://onlinejudge.u-aizu.ac.jp/courses/library/3/DSL/2/DSL_2_F
 template <class Monoid, class Function>
 class LazySegmentTree {
   using OperatorMonoid = Monoid;
