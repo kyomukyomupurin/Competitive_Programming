ac9ad93a8432cd938d6ba141594db9c750dcc9fa
kyomukyomupurin <murakami1728@gmail.com>
2020-01-17 13:22:27 +0900

    initial commit

---
+++ src/etc/pbds_tree.cc
@@ -0,0 +1,8 @@
+// include __gnu_pbds::tree
+
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+#include <ext/pb_ds/tag_and_trait.hpp>
+
+template<class T> 
+using pbds_tree = __gnu_pbds::tree<T, __gnu_pbds::null_type, std::less<T>, __gnu_pbds::rb_tree_tag, __gnu_pbds::tree_order_statistics_node_update>;
