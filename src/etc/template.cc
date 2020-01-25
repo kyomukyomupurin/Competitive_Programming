41b3d0996e157506a85a663013cf8087aea83b2c
kyomukyomupurin <murakami1728@gmail.com>
2020-01-26 02:09:08 +0900

    :sparkles:

--- src/etc/template.cc
+++ src/etc/template.cc
@@ -29,7 +29,12 @@ using int64 = long long;
 
 template<class T> std::ostream& operator<<(std::ostream& os, const std::vector<T>& vec) { os << '{'; size_t n = vec.size(); for (size_t i = 0; i < n; ++i) { os << vec[i]; if (i != n - 1) os << ','; } os << '}'; return os; }
 template<class T, class U> std::ostream& operator<<(std::ostream& os, const std::pair<T, U>& p) { return os << '{' << p.first << " " << p.second << '}'; }
+template<class T, class U> std::ostream& operator<<(std::ostream& os, const std::map<T, U>& mp) { for (auto it = mp.begin(); it != mp.end(); ++it) { os << '{' << it->first << ':' << it->second << '}'; if (it != --mp.end()) os << ','; } return os; }
+template<class T> std::ostream& operator<<(std::ostream& os, const std::set<T>& st) { os << '{'; for (auto it = st.begin(); it != st.end(); ++it) { os << *it; if (it != --st.end()) os << ','; } os << '}'; return os; }
 template<class T> std::istream& operator>>(std::istream& is, std::vector<T>& vec) { size_t n = vec.size(); for (size_t i = 0; i < n; ++i) is >> vec[i]; return is; }
+
+#define all(_) begin(_), end(_)
+#define rall(_) rbegin(_), rend(_)
 
 #ifdef LOCAL
 #define debug(_) cerr << #_ << ": " << (_) << '\n'
