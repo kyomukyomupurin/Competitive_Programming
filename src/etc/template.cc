f2061b32596e5990399f4e4cf029667b54f32b6d
kyomukyomupurin <murakami1728@gmail.com>
2020-01-30 15:05:27 +0900

    :sparkles:

--- src/etc/template.cc
+++ src/etc/template.cc
@@ -29,7 +29,7 @@ using int64 = long long;
 
 template<class T> std::ostream& operator<<(std::ostream& os, const std::vector<T>& vec) { os << '{'; size_t n = vec.size(); for (size_t i = 0; i < n; ++i) { os << vec[i]; if (i != n - 1) os << ','; } os << '}'; return os; }
 template<class T, class U> std::ostream& operator<<(std::ostream& os, const std::pair<T, U>& p) { return os << '{' << p.first << " " << p.second << '}'; }
-template<class T, class U> std::ostream& operator<<(std::ostream& os, const std::map<T, U>& mp) { for (auto it = mp.begin(); it != mp.end(); ++it) { os << '{' << it->first << ':' << it->second << '}'; if (it != --mp.end()) os << ','; } return os; }
+template<class T, class U> std::ostream& operator<<(std::ostream& os, const std::map<T, U>& mp) { os << '{'; for (auto it = mp.begin(); it != mp.end(); ++it) { os << '{' << it->first << ':' << it->second << '}'; if (it != --mp.end()) os << ','; } os << '}'; return os; }
 template<class T> std::ostream& operator<<(std::ostream& os, const std::set<T>& st) { os << '{'; for (auto it = st.begin(); it != st.end(); ++it) { os << *it; if (it != --st.end()) os << ','; } os << '}'; return os; }
 template<class T> std::istream& operator>>(std::istream& is, std::vector<T>& vec) { size_t n = vec.size(); for (size_t i = 0; i < n; ++i) is >> vec[i]; return is; }
 
@@ -37,10 +37,19 @@ template<class T> std::istream& operator>>(std::istream& is, std::vector<T>& vec
 #define rall(_) rbegin(_), rend(_)
 
 #ifdef LOCAL
-#define debug(_) cerr << #_ << ": " << (_) << '\n'
+#define debug(...) cerr << "[" << #__VA_ARGS__ << "]: ", debug_out(__VA_ARGS__)
 #else
-#define debug(_) 1728
+#define debug(...) 1728
 #endif
+
+void debug_out() { std::cerr << '\n'; }
+
+template <class Head, class... Tail>
+void debug_out(Head&& head, Tail&&... tail) {
+  std::cerr << head;
+  if (sizeof...(Tail) != 0) std::cerr << ", ";
+  debug_out(std::forward<Tail>(tail)...);
+}
 
 int main() {
   ios_base::sync_with_stdio(false);
