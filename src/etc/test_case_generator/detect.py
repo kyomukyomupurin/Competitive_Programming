5ebc49dab872f1391cc80d1e8296f6071d5100ca
kyomukyomupurin <murakami1728@gmail.com>
2020-02-20 17:30:48 +0900

    :sparkles:

--- src/etc/test_case_generator/detect.py
+++ src/etc/test_case_generator/detect.py
@@ -4,23 +4,6 @@ import sys
 
 
 def main():
-    """
-
-    Need:
-        sol.cc    ... my solution
-        stupid.cc ... naive solution
-        gen.cc    ... random input generator
-
-    Usage:
-        'python detect.py 200'
-        args is a number of trials
-
-    Output:
-        out1.txt ... output of sol.exe
-        out2.txt ... output of stupid.exe
-        test.txt ... generated random case
-
-    """
     print("Compiling 'sol.cc'...")
     subprocess.run(['g++', '-O2', '-DLOCAL', '-std=c++17', 'sol.cc', '-o', 'sol'])
     print("Compiled 'sol.cc'")
@@ -40,10 +23,17 @@ def main():
         subprocess.run('./sol.exe', stdin=open('./test.txt'), stdout=open('./out1.txt', 'w'))
         subprocess.run('./stupid.exe', stdin=open('./test.txt'), stdout=open('./out2.txt', 'w'))
         if not filecmp.cmp('./out1.txt', './out2.txt'):
+            print("[test {:<3}] : WA".format(i + 1))
             print('Wrong answer on test {:<3}'.format(i + 1))
+            print('------------------------------------------------\n')
+            with open('./test.txt', 'r') as corner_case:
+                lines = corner_case.readlines()
+                for line in lines:
+                    print(line)
+            print('------------------------------------------------')
             exit(0)
         else:
-            print("test {:<3} : ok".format(i + 1))
+            print("[test {:<3}] : AC".format(i + 1))
 
 if __name__ == '__main__':
     main()
