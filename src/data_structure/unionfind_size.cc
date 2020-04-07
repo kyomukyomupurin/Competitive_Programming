b08b095f294333c8c2f1654a28a8d266f7a44098
kyomukyomupurin <murakami1728@gmail.com>
2020-04-07 20:18:22 +0900

    :sparkles:

--- src/data_structure/unionfind_size.cc
+++ src/data_structure/unionfind_size.cc
@@ -3,23 +3,29 @@
 //     https://atcoder.jp/contests/abc120/tasks/abc120_d
 
 #include <vector>
+#include <cassert>
 
 class UnionFind {
  public:
-  UnionFind(int n) : n_(n) { Initialize(); }
+  UnionFind(int n) : n_(n) { initialize(); }
 
-  int GetRoot(int x) {
+  int root(int x) {
+    assert(0 <= x && x < n_);
     if (parent_[x] < 0)
       return x;
     else
-      return parent_[x] = GetRoot(parent_[x]);
+      return parent_[x] = root(parent_[x]);
   }
 
-  bool IsSame(int x, int y) { return GetRoot(x) == GetRoot(y); }
+  bool same(int x, int y) {
+    assert(0 <= x && x < n_ && 0 <= y && y < n_);
+    return root(x) == root(y);
+  }
 
-  void Unite(int x, int y) {
-    x = GetRoot(x);
-    y = GetRoot(y);
+  void unite(int x, int y) {
+    assert(0 <= x && x < n_ && 0 <= y && y < n_);
+    x = root(x);
+    y = root(y);
     if (x == y) return;
     if (parent_[x] > parent_[y]) std::swap(x, y);
     parent_[x] += parent_[y];
@@ -27,11 +33,57 @@ class UnionFind {
     return;
   }
 
-  int GetSize(int x) { return -parent_[GetRoot(x)]; }
+  int size(int x) {
+    assert(0 <= x && x < n_);
+    return -parent_[root(x)];
+  }
 
  private:
   const int n_;
   std::vector<int> parent_;
 
-  void Initialize() { parent_.assign(n_, -1); }
+  void initialize() { parent_.assign(n_, -1); }
 };
+
+// verification code
+/*
+void ABC120_D() {
+  int n, m;
+  cin >> n >> m;
+  int64 ans = int64{n} * int64{n - 1} / 2;
+
+  UnionFind uf(n);
+
+  vector<pair<int, int>> in;
+
+  for (int i = 0; i < m; ++i) {
+    int a, b;
+    cin >> a >> b;
+    --a;
+    --b;
+    in.emplace_back(a, b);
+  }
+
+  reverse(in.begin(), in.end());
+
+  vector<int64> v;
+
+  v.emplace_back(ans);
+
+  for (int i = 0; i < m; ++i) {
+    if (uf.same(in[i].first, in[i].second)) {
+      ;
+    } else {
+      ans -= uf.size(in[i].first) * uf.size(in[i].second);
+      uf.unite(in[i].first, in[i].second);
+    }
+    v.emplace_back(ans);
+  }
+
+  reverse(v.begin(), v.end());
+
+  for (int i = 1; i < m + 1; ++i) {
+    cout << v[i] << '\n';
+  }
+}
+*/
