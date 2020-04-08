29434a335fb2d3c874c9cbe91bdf947909800212
kyomukyomupurin <murakami1728@gmail.com>
2020-04-09 00:07:11 +0900

    :sparkles:

--- src/graph/bellman_ford.cc
+++ src/graph/bellman_ford.cc
@@ -5,27 +5,34 @@
 #include <limits>
 #include <vector>
 
-template <class T>
+template <class _Tp>
 class BellmanFord {
  public:
-  BellmanFord(int node_size) : node_size_(node_size) { Initialize(); }
+  static constexpr _Tp kInfinity =
+      std::numeric_limits<_Tp>::max() / static_cast<_Tp>(2);
 
-  void AddEdge(int from, int to, T cost) { es_.push_back({from, to, cost}); }
+  BellmanFord(int node_size) : node_size_(node_size) { initialize(); }
+
+  void add(int from, int to, _Tp cost) {
+    assert(0 <= from && from < node_size_ && 0 <= to && to < node_size_);
+    es_.emplace_back((edge){from, to, cost});
+  }
 
   // use after bf.solve(source)
-  bool ExistNegativeCycle() {
-    for (const auto &e : es_) {
-      if (distance_[e.from] == std::numeric_limits<T>::max() / 2) continue;
+  bool find_negative_cycle() const {
+    for (const edge& e : es_) {
+      if (distance_[e.from] == kInfinity) continue;
       if (distance_[e.from] + e.cost < distance_[e.to]) return true;
     }
     return false;
   }
 
-  void Solve(int source) {
+  void solve(int source) {
+    assert(0 <= source && source < node_size_);
     distance_[source] = 0;
     for (int i = 0; i < node_size_ - 1; ++i) {
-      for (const auto &e : es_) {
-        if (distance_[e.from] == std::numeric_limits<T>::max() / 2) continue;
+      for (const edge& e : es_) {
+        if (distance_[e.from] == kInfinity) continue;
         if (distance_[e.to] > distance_[e.from] + e.cost) {
           distance_[e.to] = distance_[e.from] + e.cost;
         }
@@ -33,39 +40,42 @@ class BellmanFord {
     }
   }
 
-  T operator[](int to) const { return distance_[to]; }
+  _Tp operator[](int to) const {
+    assert(0 <= to && to < node_size_);
+    return distance_[to];
+  }
 
  private:
   struct edge {
     int from, to;
-    T cost;
+    _Tp cost;
   };
   int node_size_;
-  std::vector<T> distance_;
+  std::vector<_Tp> distance_;
   std::vector<edge> es_;
 
-  void Initialize() {
-    distance_.assign(node_size_, std::numeric_limits<T>::max() / 2);
-  }
+  void initialize() { distance_.assign(node_size_, kInfinity); }
 };
 
 // verification code
 /*
 void GRL_1_B() {
-  int n, m, r; cin >> n >> m >> r;
+  int n, m, r;
+  cin >> n >> m >> r;
   BellmanFord<int> bf(n);
   for (int i = 0; i < m; ++i) {
-    int s, t, d; cin >> s >> t >> d;
-    bf.AddEdge(s, t, d);
+    int s, t, d;
+    cin >> s >> t >> d;
+    bf.add(s, t, d);
   }
 
-  bf.Solve(r);
+  bf.solve(r);
 
-  if (bf.ExistNegativeCycle()) {
+  if (bf.find_negative_cycle()) {
     cout << "NEGATIVE CYCLE" << '\n';
   } else {
     for (int i = 0; i < n; ++i) {
-      if (bf[i] == std::numeric_limits<int>::max() / 2) {
+      if (bf[i] == bf.kInfinity) {
         cout << "INF" << '\n';
       } else {
         cout << bf[i] << '\n';
