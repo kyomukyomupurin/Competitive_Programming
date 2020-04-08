99eaf93453cfd7ae5f43a1ca8320233a0b3fa8ed
kyomukyomupurin <murakami1728@gmail.com>
2020-04-08 15:32:50 +0900

    :sparkles:

--- src/graph/dinic.cc
+++ src/graph/dinic.cc
@@ -7,25 +7,29 @@
 #include <queue>
 #include <vector>
 
-template <class T>
+template <class _Tp>
 class Dinic {
  public:
-  Dinic(int node_size) : node_size_(node_size) { Initialize(); }
+  Dinic(int node_size) : node_size_(node_size) { initialize(); }
 
-  void AddEdge(int from, int to, T cost) {
-    graph_[from].push_back({to, cost, static_cast<int>(graph_[to].size())});
-    graph_[to].push_back(
-        {from, static_cast<T>(0), static_cast<int>(graph_[from].size()) - 1});
+  void add(int from, int to, _Tp cost) {
+    assert(0 <= from && from < node_size_ && 0 <= to && to < node_size_);
+    graph_[from].emplace_back(
+        (edge){to, cost, static_cast<int>(graph_[to].size())});
+    graph_[to].emplace_back((edge){from, static_cast<_Tp>(0),
+                                   static_cast<int>(graph_[from].size()) - 1});
   }
 
-  T MaxFlow(int s, int t) {
-    T flow = 0;
+  _Tp max_flow(int s, int t) {
+    _Tp flow = 0;
     for (;;) {
-      BFS(s);
+      bfs(s);
       if (level_[t] < 0) return flow;
       std::fill(iter_.begin(), iter_.end(), 0);
-      T f;
-      while ((f = DFS(s, t, std::numeric_limits<T>::max() / 2)) > 0) {
+      _Tp f;
+      while ((f = dfs(s, t,
+                      std::numeric_limits<_Tp>::max() / static_cast<_Tp>(2))) >
+             0) {
         flow += f;
       }
     }
@@ -34,7 +38,7 @@ class Dinic {
  private:
   struct edge {
     int to;
-    T cap;
+    _Tp cap;
     int rev;
   };
   int node_size_;
@@ -42,35 +46,35 @@ class Dinic {
   std::vector<int> level_;
   std::vector<int> iter_;
 
-  void Initialize() {
+  void initialize() {
     graph_.resize(node_size_);
     level_.assign(node_size_, 0);
     iter_.assign(node_size_, 0);
   }
 
-  void BFS(int s) {
+  void bfs(int s) {
     std::fill(level_.begin(), level_.end(), -1);
-    std::queue<int> q;
+    std::queue<int> que;
     level_[s] = 0;
-    q.push(s);
-    while (!q.empty()) {
-      int top = q.front();
-      q.pop();
-      for (const edge& next : graph_[top]) {
+    que.emplace(s);
+    while (!que.empty()) {
+      int cur = que.front();
+      que.pop();
+      for (const edge& next : graph_[cur]) {
         if (next.cap > 0 && level_[next.to] < 0) {
-          level_[next.to] = level_[top] + 1;
-          q.push(next.to);
+          level_[next.to] = level_[cur] + 1;
+          que.emplace(next.to);
         }
       }
     }
   }
 
-  T DFS(int v, int t, int f) {
+  _Tp dfs(int v, int t, int f) {
     if (v == t) return f;
-    for (int& i = iter_[v]; i < graph_[v].size(); ++i) {
+    for (int& i = iter_[v]; i < (int)graph_[v].size(); ++i) {
       edge& e = graph_[v][i];
       if (e.cap > 0 && level_[v] < level_[e.to]) {
-        T d = DFS(e.to, t, min(f, e.cap));
+        _Tp d = dfs(e.to, t, min(f, e.cap));
         if (d > 0) {
           e.cap -= d;
           graph_[e.to][e.rev].cap += d;
@@ -85,19 +89,22 @@ class Dinic {
 // verification code
 /*
 void GRL_6_A() {
-  int n, m; cin >> n >> m;
+  int n, m;
+  cin >> n >> m;
   Dinic<int> dinic(n);
   for (int i = 0; i < m; ++i) {
-    int u, v, c; cin >> u >> v >> c;
-    dinic.AddEdge(u, v, c);
+    int u, v, c;
+    cin >> u >> v >> c;
+    dinic.add(u, v, c);
   }
-  cout << dinic.MaxFlow(0, n - 1) << '\n';
+  cout << dinic.max_flow(0, n - 1) << '\n';
 }
 */
 
 /*
 void ARC092_A() {
-  int n; cin >> n;
+  int n;
+  cin >> n;
   vector<int> a(n), b(n), c(n), d(n);
   for (int i = 0; i < n; ++i) {
     cin >> a[i] >> b[i];
@@ -110,21 +117,21 @@ void ARC092_A() {
   int s = 2 * n, t = 2 * n + 1;
 
   for (int i = 0; i < n; ++i) {
-    dinic.AddEdge(s, i, 1);
+    dinic.add(s, i, 1);
   }
 
   for (int i = 0; i < n; ++i) {
-    dinic.AddEdge(n + i, t, 1);
+    dinic.add(n + i, t, 1);
   }
 
   for (int i = 0; i < n; ++i) {
     for (int j = 0; j < n; ++j) {
       if (a[i] < c[j] && b[i] < d[j]) {
-        dinic.AddEdge(i, j + n, 1);
+        dinic.add(i, j + n, 1);
       }
     }
   }
 
-  cout << dinic.MaxFlow(s, t) << '\n';
+  cout << dinic.max_flow(s, t) << '\n';
 }
 */
