5bc887524447e1c7a02fec0d5a330379228cacf6
kyomukyomupurin <murakami1728@gmail.com>
2020-04-08 14:45:23 +0900

    :sparkles:

--- src/math/combination.cc
+++ src/math/combination.cc
@@ -1,48 +1,60 @@
 // nCr (mod 1000000007)
-// compile time calculation version
-// warning : it takes much more compile time and sometimes ends up with compilation error !!
-// if n_ >= 10^6, remove "constexpr"
+// verified by
+//     https://atcoder.jp/contests/abc154/tasks/abc154_f
 
 #include <iostream>
+#include <array>
 
 using int64 = long long;
 
 class Combination {
  public:
-  constexpr Combination() { Build(); }
   static constexpr int mod_ = 1000000007;
+  static constexpr int n_ = 2000000 + 5;
 
-  #ifdef LOCAL
-  static constexpr int n_ = 1000 + 5;
-  #else
-  static constexpr int n_ = 200000 + 5;
-  #endif
+  Combination() { build(); }
 
-  struct LookupTable {
-    int64 factorial_[n_];
-    int64 inverse_[n_];
-    int64 finverse_[n_];
-  };
-  LookupTable lt{};
+  std::array<int64, n_> fact_;
+  std::array<int64, n_> inv_;
+  std::array<int64, n_> finv_;
 
   // return nCk
-  constexpr int64 Get(int n, int k) const noexcept {
+  int64 get(int n, int k) const noexcept {
     if (n < k || n < 0 || k < 0) return 0;
-    return lt.factorial_[n] * (lt.finverse_[k] * lt.finverse_[n - k] % mod_) %
-           mod_;
+    return fact_[n] * (finv_[k] * finv_[n - k] % mod_) % mod_;
   }
 
  private:
-  constexpr void Build() noexcept {
-    lt.factorial_[0] = 1;
-    lt.factorial_[1] = 1;
-    lt.finverse_[0] = 1;
-    lt.finverse_[1] = 1;
-    lt.inverse_[1] = 1;
+  void build() noexcept {
+    fact_[0] = 1;
+    fact_[1] = 1;
+    finv_[0] = 1;
+    finv_[1] = 1;
+    inv_[1] = 1;
     for (int i = 2; i < n_; ++i) {
-      lt.factorial_[i] = lt.factorial_[i - 1] * i % mod_;
-      lt.inverse_[i] = mod_ - lt.inverse_[mod_ % i] * (mod_ / i) % mod_;
-      lt.finverse_[i] = lt.finverse_[i - 1] * lt.inverse_[i] % mod_;
+      fact_[i] = fact_[i - 1] * i % mod_;
+      inv_[i] = mod_ - inv_[mod_ % i] * (mod_ / i) % mod_;
+      finv_[i] = finv_[i - 1] * inv_[i] % mod_;
     }
   }
 };
+
+// verification code
+/*
+void ABC_154_F() {
+  int64 r1, c1, r2, c2;
+  cin >> r1 >> c1 >> r2 >> c2;
+
+  int64 ans = 0;
+
+  Combination kyomu;
+
+  for (int64 i = r1; i <= r2; ++i) {
+    ans += (((c2 + 1LL) * kyomu.get(c2 + i + 1LL, i) % kyomu.mod_) * kyomu.inv_[i + 1]) % kyomu.mod_ - ((c1 * kyomu.get(c1 + i, i) % kyomu.mod_) * kyomu.inv_[i + 1] % kyomu.mod_);
+    while (ans < 0) ans += kyomu.mod_;
+    ans %= kyomu.mod_;
+  }
+
+  cout << ans << endl;
+}
+*/
