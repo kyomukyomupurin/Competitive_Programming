2e36b5c3b8531ae4edea781aaa89007adc8b428c
kyomukyomupurin <murakami1728@gmail.com>
2020-04-08 15:18:45 +0900

    :sparkles:

--- src/math/prime_factorization.cc
+++ src/math/prime_factorization.cc
@@ -1,28 +1,14 @@
 // Prime Factorization
 
 #include <iostream>
-#include <vector>
 #include <map>
 
 using int64 = long long;
 
-std::vector<std::pair<int64, int64>> prime_fact(int64 n){
-  std::vector<std::pair<int64, int64>> result;
-  for (int64 i = 2; i * i <= n; ++i){
-    if (n % i != 0) continue;
-    int cnt = 0;
-    while (n % i == 0){
-      n /= i; ++cnt;
-    }
-    result.emplace_back(i, cnt);
-  }
-  if (n != 1) result.emplace_back(n, 1);
-  return result;
-}
-
-std::map<int64, int> prime_fact_(int64 n){
-  std::map<int64, int> result;
-  for (int64 i = 2; i * i <= n; ++i){
+template<class _Tp>
+std::map<_Tp, int> prime_fact(_Tp n){
+  std::map<_Tp, int> result;
+  for (_Tp i = 2; i * i <= n; ++i){
     if (n % i != 0) continue;
     int cnt = 0;
     while (n % i == 0){
