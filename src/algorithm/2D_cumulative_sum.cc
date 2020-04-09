cd47b182b736e7aa76f8453e418fd2fc10f9d0b8
kyomukyomupurin <murakami1728@gmail.com>
2020-04-09 15:05:15 +0900

    :sparkles:

--- src/algorithm/2D_cumulative_sum.cc
+++ src/algorithm/2D_cumulative_sum.cc
@@ -5,38 +5,38 @@
 #include <vector>
 #include <cassert>
 
-template <class T>
+template <class _Tp>
 class CumulativeSum2D {
  public:
-  CumulativeSum2D(const std::vector<vector<T>>& vec)
-      : height_(vec.size()), width_(vec[0].size()), vec_(vec) {
-    Build();
+  CumulativeSum2D(const std::vector<vector<_Tp>>& vec)
+      : x_(vec.size()), y_(vec[0].size()), vec_(vec) {
+    build();
   }
 
   // return sum of [sx, gx] * [sy, gy]
-  T Get(int sx, int sy, int gx, int gy) {
+  _Tp get(int sx, int sy, int gx, int gy) {
     assert(sx <= gx && sy <= gy);
-    assert(0 <= sx && sx < (int) height_ && 0 <= gx && gx < (int) height_ && 0 <= sy &&
-          sy < (int) width_ && 0 <= gy && gy < (int) width_);
+    assert(0 <= sx && sx < x_ && 0 <= gx && gx < x_ && 0 <= sy && sy < y_ &&
+           0 <= gy && gy < y_);
     return sum_[gx + 1][gy + 1] - sum_[gx + 1][sy] - sum_[sx][gy + 1] +
-          sum_[sx][sy];
+           sum_[sx][sy];
   }
 
  private:
-  size_t height_;
-  size_t width_;
-  std::vector<vector<T>> vec_;
-  std::vector<vector<T>> sum_;
+  int x_;
+  int y_;
+  std::vector<vector<_Tp>> vec_;
+  std::vector<vector<_Tp>> sum_;
 
-  void Build() {
-    sum_.assign(height_ + 1, vector<T>(width_ + 1, static_cast<T>(0)));
-    for (size_t i = 0; i < height_; ++i) {
-      for (size_t j = 0; j < width_; ++j) {
+  void build() {
+    sum_.assign(x_ + 1, vector<_Tp>(y_ + 1, static_cast<_Tp>(0)));
+    for (int i = 0; i < x_; ++i) {
+      for (int j = 0; j < y_; ++j) {
         sum_[i + 1][j + 1] = sum_[i + 1][j] + vec_[i][j];
       }
     }
-    for (size_t i = 0; i < width_; ++i) {
-      for (size_t j = 0; j < height_; ++j) {
+    for (int i = 0; i < y_; ++i) {
+      for (int j = 0; j < x_; ++j) {
         sum_[j + 1][i + 1] += sum_[j][i + 1];
       }
     }
@@ -46,19 +46,26 @@ class CumulativeSum2D {
 // verification code
 /*
 void ABC106_D() {
-  int n, m, query; cin >> n >> m >> query;
+  int n, m, query;
+  cin >> n >> m >> query;
   vector<vector<int>> board(n, vector<int>(n));
   for (int i = 0; i < m; ++i) {
-    int l, r; cin >> l >> r; --l; --r;
+    int l, r;
+    cin >> l >> r;
+    --l;
+    --r;
     ++board[l][r];
   }
 
   CumulativeSum2D<int> cs2(board);
 
   for (int i = 0; i < query; ++i) {
-    int p, q; cin >> p >> q;
-    --p; --q;
-    cout << cs2.Get(p, p, q, q) << '\n';
+    int p, q;
+    cin >> p >> q;
+    --p;
+    --q;
+    cout << cs2.get(p, p, q, q) << '\n';
   }
 }
+
 */
