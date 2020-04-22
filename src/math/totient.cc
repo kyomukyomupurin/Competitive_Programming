4a78899bb3356aff3a44bdb656cbd55ea1694495
kyomukyomupurin <murakami1728@gmail.com>
2020-04-22 16:35:19 +0900

    :tada:

---
+++ src/math/totient.cc
@@ -0,0 +1,15 @@
+// Euler's phi function
+// verified by
+//     https://onlinejudge.u-aizu.ac.jp/courses/library/6/NTL/1/NTL_1_D
+
+template <class _Tp>
+_Tp Totient(_Tp n) {
+  _Tp res = n;
+  for (_Tp i = 2; i * i <= n; ++i) {
+    if (n % i != 0) continue;
+    (res /= i) *= (i - 1);
+    while (n % i == 0) n /= i;
+  }
+  if (n != 1) (res /= n) *= (n - 1);
+  return res;
+}
