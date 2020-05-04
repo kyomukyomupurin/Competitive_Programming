8e2257fe52e636f9398f093c546ab24a08f4bc74
kyomukyomupurin <murakami1728@gmail.com>
2020-05-04 16:02:17 +0900

    :art:

--- src/math/miller_rabin.cc
+++ src/math/miller_rabin.cc
@@ -12,6 +12,24 @@ class MillerRabinTest {
 
  public:
   MillerRabinTest() {}
+
+  bool isprime(uint64 n) {
+    if (n < 2) return false;
+    int r = 0;
+    uint64 d = n - 1;
+    while ((d & 1) == 0) {
+      d >>= 1;
+      r++;
+    }
+
+    for (int a : {2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37}) {
+      if (n == (uint64)a) return true;
+      if (check(n, a, d, r)) return false;
+    }
+    return true;
+  }
+
+ private:
   uint64 power(uint64 base, uint64 e, uint64 mod) {
     uint64 res = 1;
     base %= mod;
@@ -32,20 +50,4 @@ class MillerRabinTest {
     }
     return true;
   };
-
-  bool isprime(uint64 n) {
-    if (n < 2) return false;
-    int r = 0;
-    uint64 d = n - 1;
-    while ((d & 1) == 0) {
-      d >>= 1;
-      r++;
-    }
-
-    for (int a : {2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37}) {
-      if (n == (uint64)a) return true;
-      if (check(n, a, d, r)) return false;
-    }
-    return true;
-  }
 };
