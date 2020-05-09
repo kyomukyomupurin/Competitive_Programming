cf6dc94b109d50166455ad09fd4793e69c0c8990
kyomukyomupurin <murakami1728@gmail.com>
2020-05-09 15:10:29 +0900

    :sparkles:

--- src/algorithm/cumulative_sum.cc
+++ src/algorithm/cumulative_sum.cc
@@ -10,6 +10,36 @@ class CumulativeSum {
   // return sum of [l, r]
   _Tp get(int l, int r) { return sum_[r + 1] - sum_[l]; }
 
+  // return the first k where sum of [0, k] >= value
+  // use only when all element in vec >= 0
+  int lower_bound(_Tp value) {
+    int high = vec_.size(), low = -1;
+    while (high - low > 1) {
+      int mid = (high + low) >> 1;
+      if (get(0, mid) < value) {
+        low = mid;
+      } else {
+        high = mid;
+      }
+    }
+    return high;
+  }
+
+  // return the first k where sum of [0, k] > value
+  // use only when all element in vec >= 0
+  int upper_bound(_Tp value) {
+    int high = vec_.size(), low = -1;
+    while (high - low > 1) {
+      int mid = (high + low) >> 1;
+      if (get(0, mid) <= value) {
+        low = mid;
+      } else {
+        high = mid;
+      }
+    }
+    return high;
+  }
+
  private:
   std::vector<_Tp> vec_;
   std::vector<_Tp> sum_;
