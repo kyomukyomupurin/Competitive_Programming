d80b328638a4617066990d5f25e0b1f886e2ed78
kyomukyomupurin <murakami1728@gmail.com>
2020-06-21 17:40:32 +0900

    :sparkles:

--- src/etc/fast_input.cc
+++ src/etc/fast_input.cc
@@ -5,16 +5,36 @@
 #include <iostream>
 
 // snippet-begin
-template<class T> void fastin(T &num) {
-  bool negative = false;
-  T c;
-  num = 0;
-  c = getchar_unlocked();
-  if (c == '-') {
-    negative = true;
-    c = getchar_unlocked();
+class Scanner {
+  static constexpr int buf = (1 << 27);
+  char in[buf], *rb;
+
+ public:
+  Scanner() {
+    std::fread(in, 1, buf, stdin);
+    rb = in;
   }
-  for (; (c > 47 && c < 58); c = getchar_unlocked()) num = (num << 3) + (num << 1) + c - 48;
-  if (negative) num *= -1;
-}
+
+  template <class Integer>
+  inline void read_int(Integer& num) {
+    bool neg = false;
+    num = 0;
+    while (!std::isdigit(*rb) && *rb != '-') ++rb;
+    if (*rb == '-') neg = true, ++rb;
+    while (std::isdigit(*rb)) num = (num << 3) + (num << 1) + (*rb - '0'), ++rb;
+    if (neg) num = -num;
+  }
+
+  inline void read_string(std::string& str) {
+    while (std::isspace(*rb)) ++rb;
+    auto it = rb;
+    while (!std::isspace(*rb)) ++rb;
+    str = std::string(it, rb);
+  }
+
+  inline void read_char(char& c) {
+    while (std::isspace(*rb)) ++rb;
+    c = *rb, ++rb;
+  }
+};
 // snippet-end
