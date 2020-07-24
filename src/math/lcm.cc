ec4ede54ff89b3bbc0d5f3e265770e475a33c735
kyomukyomupurin <murakami1728@gmail.com>
2020-07-24 17:50:44 +0900

    :sparkles:

--- src/math/lcm.cc
+++
@@ -1,4 +0,0 @@
-// GCD(Greatest Common Divisor), LCM(Least Common Multiple)
-
-template<class T> T gcd(T x, T y) { return __gcd(x, y); }
-template<class T> T lcm(T x, T y) { return x / gcd(x, y) * y; }
