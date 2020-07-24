ec26990d05a5086fff9eb58cc188029c731c1b00
kyomukyomupurin <murakami1728@gmail.com>
2020-07-24 11:50:53 +0900

    :sparkles:

--- src/graph/lca.cc
+++ src/graph/lca.cc
@@ -6,26 +6,32 @@
 #include <vector>
 #include <cassert>
 
+#include "./graph.cc"
+
 // snippet-begin
-class LCA {
+template <class T>
+class lca_forest : public forest<T> {
  public:
-  LCA(int node_size, int root = 0)
-      : node_size_(node_size),
-        log_node_(32 - __builtin_clz(node_size_)),
-        root_(root) {
+  using forest<T>::n_;
+  using forest<T>::data_;
+  using forest<T>::edges_;
+
+  lca_forest(int n, int r = 0)
+      : forest<T>(n), lg_(32 - __builtin_clz(n)), r_(r) {
     init();
   }
 
-  void add(int u, int v) {
-    assert(0 <= u && u < node_size_ && 0 <= v && v < node_size_);
-    graph_[u].emplace_back(v);
-    graph_[v].emplace_back(u);
+  void add(int from, int to, T cost = 1) {
+    int id = edges_.size();
+    data_[from].emplace_back(id);
+    data_[to].emplace_back(id);
+    edges_.push_back({from, to, cost});
   }
 
   void build() {
-    dfs(root_, -1, 0);
-    for (int i = 0; i + 1 < log_node_; ++i) {
-      for (int j = 0; j < node_size_; ++j) {
+    dfs(r_, -1, 0);
+    for (int i = 0; i + 1 < lg_; ++i) {
+      for (int j = 0; j < n_; ++j) {
         if (par_[i][j] < 0) {
           par_[i + 1][j] = -1;
         } else {
@@ -37,15 +43,15 @@ class LCA {
 
   // use after build()
   int lca(int u, int v) {
-    assert(0 <= u && u < node_size_ && 0 <= v && v < node_size_);
-    if (depth_[u] > depth_[v]) std::swap(u, v);
-    for (int i = 0; i < log_node_; ++i) {
-      if ((depth_[v] - depth_[u]) >> i & 1) {
+    assert(0 <= u && u < n_ && 0 <= v && v < n_);
+    if (dep_[u] > dep_[v]) std::swap(u, v);
+    for (int i = 0; i < lg_; ++i) {
+      if ((dep_[v] - dep_[u]) >> i & 1) {
         v = par_[i][v];
       }
     }
     if (u == v) return u;
-    for (int i = log_node_ - 1; i >= 0; --i) {
+    for (int i = lg_ - 1; i >= 0; --i) {
       if (par_[i][u] != par_[i][v]) {
         u = par_[i][u];
         v = par_[i][v];
@@ -55,31 +61,30 @@ class LCA {
   }
 
   int dist(int u, int v) {
-    assert(0 <= u && u < node_size_ && 0 <= v && v < node_size_);
-    return depth_[u] + depth_[v] - 2 * depth_[lca(u, v)];
+    assert(0 <= u && u < n_ && 0 <= v && v < n_);
+    return dep_[u] + dep_[v] - 2 * dep_[lca(u, v)];
   }
 
  private:
-  int node_size_;
-  int log_node_;
-  int root_;
-  std::vector<std::vector<int>> graph_;
+  int lg_;
+  int r_;
   std::vector<std::vector<int>> par_;
-  std::vector<int> depth_;
+  std::vector<int> dep_;
 
-  void dfs(int cur, int prev, int d) {
-    par_[0][cur] = prev;
-    depth_[cur] = d;
-    for (const int& next : graph_[cur]) {
-      if (next == prev) continue;
-      dfs(next, cur, d + 1);
+  void dfs(int cur, int pre, int d) {
+    par_[0][cur] = pre;
+    dep_[cur] = d;
+    for (int id : data_[cur]) {
+      auto& e = edges_[id];
+      int nxt = e.to ^ e.from ^ cur;
+      if (nxt == pre) continue;
+      dfs(nxt, cur, d + 1);
     }
   }
 
   void init() {
-    depth_.resize(node_size_);
-    graph_.resize(node_size_);
-    par_.assign(log_node_, std::vector<int>(node_size_, -1));
+    dep_.resize(n_);
+    par_.assign(lg_, std::vector<int>(n_, -1));
   }
 };
 // snippet-end
@@ -89,7 +94,7 @@ class LCA {
 void GRL_5_C() {
   int n;
   cin >> n;
-  LCA lca(n);
+  lca_forest<int> lca(n);
   for (int i = 0; i < n; ++i) {
     int k;
     cin >> k;
@@ -102,7 +107,7 @@ void GRL_5_C() {
   lca.build();
   int q;
   cin >> q;
-  for (int i = 0; i < q; ++i) {
+  while (q--) {
     int u, v;
     cin >> u >> v;
     cout << lca.lca(u, v) << '\n';
@@ -112,7 +117,7 @@ void GRL_5_C() {
 /*
 void ABC_14_D() {
   int n; cin >> n;
-  LCA lca(n);
+  lca_forest<int> lca(n);
   for (int i = 0; i < n - 1; ++i) {
     int x, y; cin >> x >> y;
     --x; --y;
@@ -120,7 +125,7 @@ void ABC_14_D() {
   }
   lca.build();
   int q; cin >> q;
-  for (int i = 0; i < q; ++i) {
+  while (q--) {
     int a, b; cin >> a >> b;
     --a; --b;
     cout << lca.dist(a, b) + 1 << '\n';
