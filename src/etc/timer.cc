19fd45c5c9638cc187e74a28fb7a2da25b185c3d
kyomukyomupurin <murakami1728@gmail.com>
2020-08-01 16:58:44 +0900

    :art:

--- src/etc/timer.cc
+++ src/etc/timer.cc
@@ -8,18 +8,18 @@
 class Timer {
  public:
   // (second)
-  double GetElapsedTime() {
-    return static_cast<double>(GetCycle() - start_) /
+  double get_elapsed_time() {
+    return static_cast<double>(get_cycle() - start_) /
            static_cast<double>(clock_frequency_);
   }
 
-  void Start() { start_ = GetCycle(); }
+  void start() { start_ = get_cycle(); }
 
  private:
   int64_t start_;
-  static constexpr int64_t clock_frequency_ = 2800000000;
+  static constexpr int64_t clock_frequency_ = 3000000000;
 
-  int64_t GetCycle() {
+  int64_t get_cycle() {
     uint32_t low, high;
     __asm__ volatile("rdtsc" : "=a"(low), "=d"(high));
     return (static_cast<int64_t>(low)) | (static_cast<int64_t>(high) << 32);
