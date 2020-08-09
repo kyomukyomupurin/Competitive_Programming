dbe35e4ddef185cb4765690a476026670abd05a3
kyomukyomupurin <murakami1728@gmail.com>
2020-08-09 14:39:00 +0900

    :tada:

---
+++ src/graph/grid_to_digraph.cc
@@ -0,0 +1,26 @@
+// helper function to convert grid-graph to digraph
+
+#include <string>
+#include <vector>
+
+#include "./graph.cc"
+
+digraph<int> grid_to_digraph(const std::vector<std::string>& sv) {
+  int h = sv.size(), w = sv[0].size();
+  digraph<int> g(h * w);
+  constexpr int dx[4] = {1, 0, -1, 0}, dy[4] = {0, 1, 0, -1};
+  auto Out = [&](int x, int y) { return (x < 0 || x >= h || y < 0 || y >= w); };
+  auto Compress = [&](int x, int y) { return x * w + y; };
+  for (int i = 0; i < h; ++i) {
+    for (int j = 0; j < w; ++j) {
+      for (int k = 0; k < 4; ++k) {
+        int nx = i + dx[k], ny = j + dy[k];
+        if (Out(nx, ny)) continue;
+        if (sv[i][j] == '.' && sv[nx][ny] == '.') {
+          g.add(Compress(i, j), Compress(nx, ny));
+        }
+      }
+    }
+  }
+  return g;
+}
