bfc205f22c30758bb09e6bcdaaae9b1e1aad0dec
kyomukyomupurin <murakami1728@gmail.com>
2020-08-12 21:33:13 +0900

    :art: Change the type of last argument

--- src/algorithm/golden_section_search.cc
+++ src/algorithm/golden_section_search.cc
@@ -8,16 +8,15 @@
 
 // snippet-begin
 template <class F>
-double GoldenSectionSearch(double low, double high, F f, std::string type = "min") {
-  assert(type == "min" || type == "max");
+std::pair<double, double> GoldenSectionSearch(double low, double high, F f, bool find_min = true) {
   constexpr double phi = (1 + std::sqrt(5)) / 2;
   double c1 = (phi * low + high) / (1 + phi);
   double c2 = (low + phi * high) / (1 + phi);
   double cache_f1 = f(c1), cache_f2 = f(c2);
 
   for (int i = 0; i < 150; ++i) {
-    if ((type == "min" && cache_f1 > cache_f2) ||
-        (type == "max" && cache_f1 < cache_f2)) {
+    if ((find_min && cache_f1 > cache_f2) ||
+        (!find_min && cache_f1 < cache_f2)) {
       low = c1;
       cache_f1 = cache_f2;
       c1 = c2;
@@ -32,7 +31,7 @@ double GoldenSectionSearch(double low, double high, F f, std::string type = "min
     }
   }
 
-  return (low + high) / 2;
+  return {low, cache_f1};
 }
 // snippet-end
 
@@ -41,6 +40,6 @@ double GoldenSectionSearch(double low, double high, F f, std::string type = "min
 void ARC54_B() {
   double p; cin >> p;
   auto f = [&](double x) { return x + p * (pow(0.5, (x / 1.5))); };
-  cout << fixed << setprecision(17) << f(GoldenSectionSearch(0, p, f)) << '\n';
+  cout << fixed << setprecision(17) << GoldenSectionSearch(0, p, f).second << '\n';
 }
 */
