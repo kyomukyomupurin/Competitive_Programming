5b73922d93aa0d53e6af4079c4ee86e9edd1bd36
kyomukyomupurin <murakami1728@gmail.com>
2020-08-14 16:41:49 +0900

    :tada: Initial commit

---
+++ src/etc/template_codeforces.cc
@@ -0,0 +1,47 @@
+// Template for Codeforces(multi test cases)
+
+// snippet-begin
+#include <algorithm>
+#include <bitset>
+#include <cassert>
+#include <cctype>
+#include <chrono>
+#include <cmath>
+#include <complex>
+#include <cstring>
+#include <deque>
+#include <iomanip>
+#include <iostream>
+#include <map>
+#include <numeric>
+#include <queue>
+#include <random>
+#include <set>
+#include <stack>
+#include <string>
+#include <tuple>
+#include <utility>
+#include <vector>
+
+using namespace std;
+
+using int64 = long long;
+
+#define all(_) begin(_), end(_)
+#define rall(_) rbegin(_), rend(_)
+
+int main() {
+  ios_base::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  int t; cin >> t;
+
+  auto Solve = []() -> void {
+
+  };
+
+  while (t--) Solve();
+
+  return 0;
+}
+// snippet-end
