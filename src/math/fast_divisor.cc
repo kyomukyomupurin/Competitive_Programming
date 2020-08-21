ee5d3772af32ede041a25fb09174b730eb457561
kyomukyomupurin <murakami1728@gmail.com>
2020-08-21 21:53:22 +0900

    :tada: Initial commit

---
+++ src/math/fast_divisor.cc
@@ -0,0 +1,30 @@
+// Fast divisor enumeration
+
+#include <algorithm>
+#include <vector>
+
+#include "./../algorithm/run_length_encoding.cc"
+#include "./factor.cc"
+
+template <class T>
+std::vector<T> divisor(T n) {
+  std::vector<T> v1 = factor(n);
+  auto v2 = RunLengthEncoding(v1);
+  std::vector<T> div;
+  int sz = v2.size();
+  auto dfs = [&](auto&& self, T val, int d) -> void {
+    if (d == sz) {
+      div.emplace_back(val);
+      return;
+    }
+    int cur = 1;
+    auto [pr, cnt] = v2[d];
+    for (int i = 0; i < cnt + 1; ++i, cur *= pr) {
+      self(self, val * cur, d + 1);
+    }
+  };
+  dfs(dfs, 1, 0);
+  std::sort(div.begin(), div.end());
+
+  return div;
+}
