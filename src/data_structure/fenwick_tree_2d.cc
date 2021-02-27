2c00c8aff22031c10368735619bbc8e66f3877a6
kyomukyomupurin <murakami1728@gmail.com>
2021-02-27 19:00:47 +0900

    :sparkles: Add constructor

--- src/data_structure/fenwick_tree_2d.cc
+++ src/data_structure/fenwick_tree_2d.cc
@@ -7,8 +7,17 @@ template <class T>
 class FenwickTree2D {
  public:
   FenwickTree2D(const std::vector<std::vector<T>>& vec)
-      : x_(int(vec.size())), y_(int(vec[0].size())), vec_(vec) {
-    build();
+      : x_(int(vec.size())), y_(int(vec[0].size())) {
+    node_.assign(x_ + 1, std::vector<T>(y_ + 1, 0));
+    for (int i = 0; i < x_; ++i) {
+      for (int j = 0; j < y_; ++j) {
+        add(i, j, vec[i][j]);
+      }
+    }
+  }
+
+  FenwickTree2D(int x, int y) : x_(x), y_(y) {
+    node_.assign(x_ + 1, std::vector<T>(y_ + 1, 0));
   }
 
   // add val to (x, y)
@@ -21,7 +30,7 @@ class FenwickTree2D {
   }
 
   // return sum of [0, x] * [0, y]
-  T get(int x, int y) {
+  T get(int x, int y) const {
     T sum = 0;
     for (int i = x + 1; i > 0; i -= i & -i) {
       for (int j = y + 1; j > 0; j -= j & -j) {
@@ -32,7 +41,7 @@ class FenwickTree2D {
   }
 
   // return sum of [sx, gx] * [sy, gy] 
-  T get(int sx, int sy, int gx, int gy) {
+  T get(int sx, int sy, int gx, int gy) const {
     return get(gx, gy) - get(sx - 1, gy) - get(gx, sy - 1) +
            get(sx - 1, sy - 1);
   }
@@ -40,16 +49,6 @@ class FenwickTree2D {
  private:
   int x_;
   int y_;
-  std::vector<std::vector<T>> vec_;
   std::vector<std::vector<T>> node_;
-
-  void build() {
-    node_.assign(x_ + 1, std::vector<T>(y_ + 1, 0));
-    for (int i = 0; i < x_; ++i) {
-      for (int j = 0; j < y_; ++j) {
-        add(i, j, vec_[i][j]);
-      }
-    }
-  }
 };
 // snippet-end
