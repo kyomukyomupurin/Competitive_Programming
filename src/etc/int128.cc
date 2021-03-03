94b73fb4e36c3156618739958081ccb657e21740
kyomukyomupurin <murakami1728@gmail.com>
2021-03-03 17:24:31 +0900

    :recycle: Refactor code

--- src/etc/int128.cc
+++ src/etc/int128.cc
@@ -7,76 +7,22 @@
 // snippet-begin
 using int128 = __int128_t;
 
-std::istream& operator>>(std::istream& is, int128& val) {
-  std::string str;
-  is >> str;
-  val = 0;
-  bool neg = false;
-  int sz = str.size();
-  for (int i = 0; i < sz; ++i) {
-    if (!i && str[i] == '-') {
-      neg = true;
-    } else {
-      val = val * 10 + (str[i] & 15);
-    }
-  }
-  if (neg) val = -val;
-  return is;
-}
-
-std::ostream& operator<<(std::ostream& os, const int128& val) {
-  if (!val) return os << '0';
-  int128 t = val;
-  if (val < 0) {
-    os << '-';
-    t = -t;
-  }
-  std::string str = "";
-  while (t) {
-    str += char(t % 10 | 48);
-    t /= 10;
-  }
-  std::reverse(str.begin(), str.end());
-  return os << str;
-}
-
 int128 to_int128(const std::string& str) {
-  int sz = str.size();
   int128 val = 0;
-  bool neg = false;
-  for (int i = 0; i < sz; ++i) {
-    if (!i && str[i] == '-') {
-      neg = true;
-    } else {
-      val = val * 10 + (str[i] & 15);
-    }
+  bool neg = (str[0] == '-');
+  for (int i = neg ? 1 : 0; i < int(str.size()); ++i) {
+    val = val * 10 + (str[i] & 15);
   }
-  if (neg) val = -val;
-  return val;
+  return neg ? -val : val;
 }
 
-int128 to_int128(const char* str) {
-  int128 val = 0;
-  bool neg = false;
-  for (int i = 0; str[i]; ++i) {
-    if (!i && str[i] == '-') {
-      neg = true;
-    } else {
-      val = val * 10 + (str[i] & 15);
-    }
-  }
-  if (neg) val = -val;
-  return val;
-}
+int128 to_int128(const char* str) { return to_int128(std::string(str)); }
 
 std::string to_string(const int128& val) {
   std::string str = "";
   int128 t = val;
-  bool neg = false;
-  if (t < 0) {
-    neg = true;
-    t = -t;
-  }
+  bool neg = (t < 0);
+  if (neg) t = -t;
   while (t) {
     str += char(t % 10 | 48);
     t /= 10;
@@ -84,5 +30,16 @@ std::string to_string(const int128& val) {
   if (neg) str += '-';
   std::reverse(str.begin(), str.end());
   return str;
+}
+
+std::istream& operator>>(std::istream& is, int128& val) {
+  std::string str;
+  is >> str;
+  val = to_int128(str);
+  return is;
+}
+
+std::ostream& operator<<(std::ostream& os, const int128& val) {
+  return os << to_string(val);
 }
 // snippet-end
