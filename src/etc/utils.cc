ab9675901b3199dc0e8681531e9385d261b5d4a1
kyomukyomupurin <murakami1728@gmail.com>
2021-03-10 19:29:24 +0900

    :fire: Remove compressed()

--- src/etc/utils.cc
+++ src/etc/utils.cc
@@ -10,19 +10,6 @@
 
 // snippet-begin
 template <class T>
-inline std::vector<int> compressed(const std::vector<T>& vec) {
-  std::vector<T> t = vec;
-  std::sort(t.begin(), t.end());
-  int n = vec.size();
-  t.erase(std::unique(t.begin(), t.end()), t.end());
-  std::vector<int> compressed(n);
-  for (int i = 0; i < n; ++i)
-    compressed[i] =
-        std::distance(t.begin(), std::lower_bound(t.begin(), t.end(), vec[i]));
-  return compressed;
-}
-
-template <class T>
 inline void println(T val) {
   std::cout << val << '\n';
 }
