e214e072626e49340c3590e39c1ae6f3b1ac83f7
kyomukyomupurin <murakami1728@gmail.com>
2021-03-10 19:29:08 +0900

    :tada: Initial commit

---
+++ src/algorithm/coodinate_compress.cc
@@ -0,0 +1,17 @@
+// Coodinate compression
+
+#include <algorithm>
+#include <vector>
+
+template <class T>
+inline std::vector<int> compressed(const std::vector<T>& vec) {
+  std::vector<T> t = vec;
+  std::sort(t.begin(), t.end());
+  int n = vec.size();
+  t.erase(std::unique(t.begin(), t.end()), t.end());
+  std::vector<int> compressed(n);
+  for (int i = 0; i < n; ++i)
+    compressed[i] =
+        std::distance(t.begin(), std::lower_bound(t.begin(), t.end(), vec[i]));
+  return compressed;
+}
